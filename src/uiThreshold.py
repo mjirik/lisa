f25ef1f40de49884a792d71ebdd168f0c9f4472d
Pavel Volkovinsky <volkovinsky.pavel@gmail.com>
2012-11-08 23:35:12 +0100

    uiThreshold update

--- src/uiThreshold.py
+++ src/uiThreshold.py
@@ -1,70 +1,73 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 22 19:09:40 2012
+#===============================================================================
+# Name:        uiThreshold
+# Purpose:
+#
+# Author:      Pavel Volkovinsky (volkovinsky.pavel@gmail.com)
+#
+# Created:     08.11.2012
+# Copyright:   (c) PavelVolkovinsky 2012
+# Licence:     <your licence>
+#===============================================================================
+VERSION = "0.0.1"
 
-@author: Pavel Volkovinsky
-"""
+try:
+    import sys
+    sys.path.append("../src/")
+    sys.path.append("../extern/")
+    import pylab as pylab
+    import matplotlib.pyplot as matpyplot
+    import numpy as np
+    from matplotlib.widgets import Slider, Button, RadioButtons
 
-import sys
-sys.path.append("../src/")
-sys.path.append("../extern/")
+except ImportError, err:
+    print "Critical error! Couldn't load module! %s" % (err)
+    sys.exit(2)
 
-#=========
-# 0
-#=========
+def main():
 
-"""
-"""
+    # Vyzve uzivatele k zadani jmena souboru.
+    info = raw_input("Give me a filename: ")
+    if(info == 'x'):
+        fileName = 'morpho.png'
+    else:
+        fileName = info
 
-#=========
-# 1
-#=========
+    imgA = pylab.imread(fileName)
+    #generates a RGB image, so do
+    #pylab.imshow(imgA)
+    imgB = pylab.mean(imgA, 2) # to get a 2-D array
+    #pylab.imshow(imgB)
+    pylab.gray()
 
-import pylab as pylab
+    fig = matpyplot.figure()
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(left = 0.05, bottom = 0.25)
+    min0 = 0
+    max0 = 10000
 
-# Vyzve uzivatele k zadani jmena souboru.
-info = raw_input("Give me an order: ")
-if(info == 'x'):
-    fileName = 'morpho.png'
-else:
-    fileName = info
+    #im = max0 * np.random.random((10,10))
+    im1 = ax.imshow(imgA) # tady byl 'im' (o radek vyse)
+    fig.colorbar(im1)
 
-imgA = pylab.imread(fileName)
-#generates a RGB image, so do
-#pylab.imshow(imgA)
-imgB = pylab.mean(imgA, 2) # to get a 2-D array
-#pylab.imshow(imgB)
-pylab.gray()
+    axcolor = 'lightgoldenrodyellow'
+    axmin = fig.add_axes([0.15, 0.1, 0.65, 0.03], axisbg = axcolor)
+    axmax  = fig.add_axes([0.15, 0.15, 0.65, 0.03], axisbg = axcolor)
 
-import matplotlib.pyplot as matpyplot
-import numpy as np
-from matplotlib.widgets import Slider, Button, RadioButtons
+    smin = Slider(axmin, 'Min', 0, 20000, valinit = min0)
+    smax = Slider(axmax, 'Max', 0, 20000, valinit = max0)
 
-fig = matpyplot.figure()
-ax = fig.add_subplot(111)
-fig.subplots_adjust(left=0.25, bottom=0.25)
-min0 = 0
-max0 = 20000
+    def update(val):
+        #=========!!!!!!!!!
+        pass
+        #=========!!!!!!!!!
+        im1.set_clim([smin.val, smax.val])
+        fig.canvas.draw()
 
-#im = max0 * np.random.random((10,10))
-im1 = ax.imshow(imgB) # tady byl 'im' (o radek vyse)
-fig.colorbar(im1)
+    smin.on_changed(update)
+    smax.on_changed(update)
 
-axcolor = 'lightgoldenrodyellow'
-axmin = fig.add_axes([0.15, 0.1, 0.65, 0.03], axisbg=axcolor)
-axmax  = fig.add_axes([0.15, 0.15, 0.65, 0.03], axisbg=axcolor)
-
-smin = Slider(axmin, 'Min', 0, 20000, valinit=min0)
-smax = Slider(axmax, 'Max', 0, 20000, valinit=max0)
-
-def update(val):
-    pass
-    im1.set_clim([smin.val,smax.val])
-    fig.canvas.draw()
-smin.on_changed(update)
-smax.on_changed(update)
-
-matpyplot.show()
+    matpyplot.show()
 
 """
 """
@@ -75,47 +78,50 @@ matpyplot.show()
 
 """
 
-from pylab import *
-from matplotlib.widgets import Slider, Button, RadioButtons
+    from pylab import *
+    from matplotlib.widgets import Slider, Button, RadioButtons
 
-ax = subplot(111)
-subplots_adjust(left=0.25, bottom=0.25)
-t = arange(0.0, 1.0, 0.001)
-a0 = 5
-f0 = 3
-s = a0*sin(2*pi*f0*t)
-l, = plot(t,s, lw=2, color='red')
-axis([0, 1, -10, 10])
+    ax = subplot(111)
+    subplots_adjust(left=0.25, bottom=0.25)
+    t = arange(0.0, 1.0, 0.001)
+    a0 = 5
+    f0 = 3
+    s = a0*sin(2*pi*f0*t)
+    l, = plot(t,s, lw=2, color='red')
+    axis([0, 1, -10, 10])
 
-axcolor = 'lightgoldenrodyellow'
-axfreq = axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
-axamp  = axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor)
+    axcolor = 'lightgoldenrodyellow'
+    axfreq = axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+    axamp  = axes([0.25, 0.15, 0.65, 0.03], axisbg=axcolor)
 
-sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
-samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
+    sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0)
+    samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
 
-def update(val):
-    amp = samp.val
-    freq = sfreq.val
-    l.set_ydata(amp*sin(2*pi*freq*t))
-    draw()
-sfreq.on_changed(update)
-samp.on_changed(update)
+    def update(val):
+        amp = samp.val
+        freq = sfreq.val
+        l.set_ydata(amp*sin(2*pi*freq*t))
+        draw()
+    sfreq.on_changed(update)
+    samp.on_changed(update)
 
-resetax = axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
-def reset(event):
-    sfreq.reset()
-    samp.reset()
-button.on_clicked(reset)
+    resetax = axes([0.8, 0.025, 0.1, 0.04])
+    button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
+    def reset(event):
+        sfreq.reset()
+        samp.reset()
+    button.on_clicked(reset)
 
-rax = axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
-radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
-def colorfunc(label):
-    l.set_color(label)
-    draw()
-radio.on_clicked(colorfunc)
+    rax = axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
+    radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
+    def colorfunc(label):
+        l.set_color(label)
+        draw()
+    radio.on_clicked(colorfunc)
 
-show()
+    show()
 
 """
+
+if __name__ == '__main__':
+    main()
