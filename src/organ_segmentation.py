daa53a88c1e2ed6e61e724d5746557dc4b6445c5
Miroslav Jiřík <miroslav.jirik@gmail.com>
2012-11-20 23:25:26 +0100

    Dicomread and Graph-Cut segmentation test

--- src/organ_segmentation.py
+++ src/organ_segmentation.py
@@ -9,6 +9,7 @@ import os
 sys.path.append("../extern/pycat/")
 sys.path.append("../extern/pycat/extern/py3DSeedEditor/")
 #import featurevector
+import unittest
 
 import logging
 logger = logging.getLogger(__name__)
@@ -17,10 +18,38 @@ logger = logging.getLogger(__name__)
 import pdb
 #  pdb.set_trace();
 #import scipy.io
+import numpy as np
 
 # ----------------- my scripts --------
 import dcmreaddata
 import pycat
+import argparse
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        """ Nastavení společných proměnných pro testy  """
+        pass
+
+    def test_readdir_and_segment(self):
+        #dcm_read_from_dir('/home/mjirik/data/medical/data_orig/46328096/')
+        data3d = dcmreaddata.dcm_read_from_dir('./../sample_data/matlab/examples/sample_data/DICOM/digest_article/')
+
+        print ("Data size: " + str(data3d.nbytes) + ', shape: ' + str(data3d.shape) )
+
+        igc = pycat.ImageGraphCut(data3d, zoom = 0.5)
+        seeds = igc.seeds
+        seeds[0,:,0] = 1
+        seeds[60:66,60:66,5:6] = 2
+        igc.noninteractivity(seeds)
+
+
+        igc.make_gc()
+        segmentation = igc.segmentation
+        self.assertTrue(segmentation[14, 4, 1] == 0)
+        self.assertTrue(segmentation[127, 120, 10] == 1)
+        self.assertTrue(np.sum(segmentation==1) > 100)
+        self.assertTrue(np.sum(segmentation==0) > 100)
+        #igc.show_segmentation()
 
 
 if __name__ == "__main__":
@@ -28,12 +57,29 @@ if __name__ == "__main__":
     #logger = logging.getLogger(__name__)
     logger = logging.getLogger()
 
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.WARNING)
     ch = logging.StreamHandler()
     logger.addHandler(ch)
 
     #logger.debug('input params')
 
+    # input parser
+    parser = argparse.ArgumentParser(description='Segment vessels from liver')
+    parser.add_argument('-d', '--debug', action='store_true',
+            help='run in debug mode')
+    parser.add_argument('-t', '--tests', action='store_true', 
+            help='run unittest')
+    args = parser.parse_args()
+
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    if args.tests:
+        # hack for use argparse and unittest in one module
+        sys.argv[1:]=[]
+        unittest.main()
+        sys.exit() 
     #dcm_read_from_dir('/home/mjirik/data/medical/data_orig/46328096/')
     data3d = dcmreaddata.dcm_read_from_dir()
 
