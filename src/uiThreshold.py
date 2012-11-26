7dba2758d8cccf46ace625d6f39090843bb284b8
Pavel Volkovinsky <volkovinsky.pavel@gmail.com>
2012-11-26 16:04:27 +0100

    uiThreshold - rozdeleni dimenzi

--- src/uiThreshold.py
+++ src/uiThreshold.py
@@ -25,6 +25,8 @@ import matplotlib.pyplot as matpyplot
 import matplotlib
 from matplotlib.widgets import Slider#, Button, RadioButtons
 from scipy import ndimage
+from scipy import misc
+import numpy
 
 """
 ================================================================================
@@ -36,84 +38,112 @@ class uiThreshold:
     def __init__(self, imgUsed):
 
         self.imgUsed = imgUsed
-        self.imgChanged1 = self.imgUsed
-        self.imgChanged2 = self.imgUsed
-        self.imgChanged3 = self.imgUsed
+        self.imgChanged = imgUsed
         
-        # Zakladni informace o obrazku (+ statisticke)
-        print('Image dtype: ', imgUsed.dtype)
-        print('Image size: ', imgUsed.size)
-        print('Image shape: ', imgUsed.shape[0], ' x ',  imgUsed.shape[1])
-        print('Max value: ', imgUsed.max(), ' at pixel ',  imgUsed.argmax())
-        print('Min value: ', imgUsed.min(), ' at pixel ',  imgUsed.argmin())
-        print('Variance: ', imgUsed.var())
-        print('Standard deviation: ', imgUsed.std())
+        inputDimension = numpy.ndim(imgUsed)
+        print('Dimension of input: ',  inputDimension)
+        if(inputDimension == 2):
+            
+            print('Sorry! Not yet implemented!\nExiting.')
         
-        # Ziskani okna (figure)
-        self.fig = matpyplot.figure()
-        # Pridani subplotu do okna (do figure)
-#        self.ax0 = self.fig.add_subplot(232)
-        self.ax1 = self.fig.add_subplot(131)
-        self.ax2 = self.fig.add_subplot(132)
-        self.ax3 = self.fig.add_subplot(133)
-        # Upraveni subplotu
-        self.fig.subplots_adjust(left = 0.1, bottom = 0.25)
-        # Vykresli obrazek
-#        self.im0 = self.ax0.imshow(imgUsed)
-        self.im1 = self.ax1.imshow(imgUsed)
-        self.im2 = self.ax2.imshow(imgUsed)
-        self.im3 = self.ax3.imshow(imgUsed)
- #       self.fig.colorbar(self.im1)
-
-        # Zakladni informace o slideru
-        axcolor = 'white' # lightgoldenrodyellow
-        axmin = self.fig.add_axes([0.25, 0.16, 0.495, 0.03], axisbg = axcolor)
-        axmax  = self.fig.add_axes([0.25, 0.12, 0.495, 0.03], axisbg = axcolor)
-        axopening = self.fig.add_axes([0.25, 0.08, 0.495, 0.03], axisbg = axcolor)
-        axclosing = self.fig.add_axes([0.25, 0.04, 0.495, 0.03], axisbg = axcolor)
-
-        # Vytvoreni slideru
-            # Minimalni pouzita hodnota v obrazku
-        min0 = imgUsed.min()
-            # Maximalni pouzita hodnota v obrazku
-        max0 = imgUsed.max()
-            # Vlastni vytvoreni slideru
-        self.smin = Slider(axmin, 'Minimal threshold', min0, max0, valinit = min0)
-        self.smax = Slider(axmax, 'Maximal threshold', min0, max0, valinit = max0)
-        self.sopen = Slider(axopening, 'Binary opening', 0, 10, valinit = 0)
-        self.sclose = Slider(axclosing, 'Binary closing', 0, 10, valinit = 0)
-
-        # Udalost pri zmene hodnot slideru - volani updatu
-        """"""
-        self.smin.on_changed(self.updateMinThreshold)
-        self.smax.on_changed(self.updateMaxThreshold)
-        self.sopen.on_changed(self.updateBinOpening)
-        self.sclose.on_changed(self.updateBinClosing)
-        
-        """
-        self.smin.on_changed(self.updateImg)
-        self.smax.on_changed(self.updateImg)
-        self.sopen.on_changed(self.updateImg)
-        self.sclose.on_changed(self.updateImg)
-        """
-        # Zobrazeni okna (plot)
-        matpyplot.show() 
+        elif(inputDimension == 3):
+            
+            """
+            self.imgChanged1 = self.imgUsed
+            self.imgChanged2 = self.imgUsed
+            self.imgChanged3 = self.imgUsed
+            """
+            
+            # Zakladni informace o obrazku (+ statisticke)
+            print('Image dtype: ', imgUsed.dtype)
+            print('Image size: ', imgUsed.size)
+            print('Image shape: ', imgUsed.shape[0], ' x ',  imgUsed.shape[1])
+            print('Max value: ', imgUsed.max(), ' at pixel ',  imgUsed.argmax())
+            print('Min value: ', imgUsed.min(), ' at pixel ',  imgUsed.argmin())
+            print('Variance: ', imgUsed.var())
+            print('Standard deviation: ', imgUsed.std())
+            
+            # Ziskani okna (figure)
+            self.fig = matpyplot.figure()
+            # Pridani subplotu do okna (do figure)
+            self.ax1 = self.fig.add_subplot(111)
+            """
+    #        self.ax0 = self.fig.add_subplot(232)
+            self.ax1 = self.fig.add_subplot(131)
+            self.ax2 = self.fig.add_subplot(132)
+            self.ax3 = self.fig.add_subplot(133)
+            """
+            # Upraveni subplotu
+            self.fig.subplots_adjust(left = 0.1, bottom = 0.25)
+            # Vykresli obrazek
+    #        self.im0 = self.ax0.imshow(imgUsed)
+            self.im1 = self.ax1.imshow(imgUsed)
+            """
+            self.im2 = self.ax2.imshow(imgUsed)
+            self.im3 = self.ax3.imshow(imgUsed)
+            """
+     #       self.fig.colorbar(self.im1)
+    
+            # Zakladni informace o slideru
+            axcolor = 'white' # lightgoldenrodyellow
+            axmin = self.fig.add_axes([0.25, 0.16, 0.495, 0.03], axisbg = axcolor)
+            axmax  = self.fig.add_axes([0.25, 0.12, 0.495, 0.03], axisbg = axcolor)
+            """
+            axopening = self.fig.add_axes([0.25, 0.08, 0.495, 0.03], axisbg = axcolor)
+            axclosing = self.fig.add_axes([0.25, 0.04, 0.495, 0.03], axisbg = axcolor)
+            """
+            
+            # Vytvoreni slideru
+                # Minimalni pouzita hodnota v obrazku
+            min0 = imgUsed.min()
+                # Maximalni pouzita hodnota v obrazku
+            max0 = imgUsed.max()
+                # Vlastni vytvoreni slideru
+            self.smin = Slider(axmin, 'Minimal threshold', min0, max0, valinit = min0)
+            self.smax = Slider(axmax, 'Maximal threshold', min0, max0, valinit = max0)
+            """
+            self.sopen = Slider(axopening, 'Binary opening', 0, 10, valinit = 0)
+            self.sclose = Slider(axclosing, 'Binary closing', 0, 10, valinit = 0)
+            """
+            
+            # Udalost pri zmene hodnot slideru - volani updatu
+            """
+            self.smin.on_changed(self.updateMinThreshold)
+            self.smax.on_changed(self.updateMaxThreshold)
+            self.sopen.on_changed(self.updateBinOpening)
+            self.sclose.on_changed(self.updateBinClosing)
+            """
+            """"""
+            self.smin.on_changed(self.updateImg)
+            self.smax.on_changed(self.updateImg)
+            """
+            self.sopen.on_changed(self.updateImg)
+            self.sclose.on_changed(self.updateImg)
+            """
+            
+            # Zobrazeni plot (figure)
+            matpyplot.show() 
+            
+        else:
+            print('Wrong input.\nDimension of input is not 2 or 3.\nExiting.')
 
     def updateImg(self, val):
         
         # Prahovani (smin, smax)
         img1 = self.imgUsed > self.smin.val
-        img2 = img1 < self.smax.val
+        self.imgChanged = img1# < self.smax.val
         
+        """
         # Binary opening a binary closing
         img3 = ndimage.binary_opening(img2)
         self.imgChanged = ndimage.binary_closing(img3)
+        """
         
         # Predani obrazku k vykresleni
         self.im1 = self.ax1.imshow(self.imgChanged)
         # Prekresleni
         self.fig.canvas.draw()
-
+    """
     def updateMinThreshold(self, val):
 
         # Prahovani
@@ -149,7 +179,7 @@ class uiThreshold:
         self.im3 = self.ax3.imshow(self.imgChanged3)
         # Prekresleni
         self.fig.canvas.draw()
-
+    """
 """
 ================================================================================
 main
@@ -161,9 +191,7 @@ if __name__ == '__main__':
 #    fileName = input('Give me a filename: ')
     # Precteni souboru (obrazku)
 #    imgLoaded = matplotlib.image.imread(fileName)
-    from scipy import misc
     imgLoaded = misc.lena()
-    imgLoaded = matplotlib.image.imread('morpho.png')
     # Vytvoreni uiThreshold
     ui = uiThreshold(imgLoaded)
 
