8541d5fad812c57b52a35085e4fd1522c40b5a43
Pavel Volkovinsky <volkovinsky.pavel@gmail.com>
2012-12-01 18:35:31 +0100

    uiThreshold - zpracovani 3D dat - vykresleni "prurezu" (jeste neni cele funkcni)

--- src/uiThreshold.py
+++ src/uiThreshold.py
@@ -13,22 +13,25 @@ Licence:     <your licence>
 """
 
 import sys
-#import argparse
 sys.path.append("../src/")
 sys.path.append("../extern/")
+
 import logging
 logger = logging.getLogger(__name__)
+
+import numpy
+
+from scipy import ndimage
+from scipy import misc
 import scipy.io
+
 import unittest
 import argparse
 #import pylab
+
 import matplotlib.pyplot as matpyplot
-#import numpy
 import matplotlib
 from matplotlib.widgets import Slider#, Button, RadioButtons
-from scipy import ndimage
-from scipy import misc
-import numpy
 
 """
 ================================================================================
@@ -37,15 +40,17 @@ uiThreshold
 """
 class uiThreshold:
 
-    def __init__(self, imgUsed, initslice = 0):
+    def __init__(self, imgUsed, initslice = 0, cmap = matplotlib.cm.Greys_r):
 
-        self.imgUsed = imgUsed
-        self.imgChanged = imgUsed
-        
         inputDimension = numpy.ndim(imgUsed)
         print('Dimension of input: ',  inputDimension)
+        self.cmap = cmap
+        
         if(inputDimension == 2):
             
+            self.imgUsed = imgUsed
+            self.imgChanged = imgUsed
+                
             """
             self.imgChanged1 = self.imgUsed
             self.imgChanged2 = self.imgUsed
@@ -53,6 +58,7 @@ class uiThreshold:
             """
             
             # Zakladni informace o obrazku (+ statisticke)
+            """
             print('Image dtype: ', imgUsed.dtype)
             print('Image size: ', imgUsed.size)
             print('Image shape: ', imgUsed.shape[0], ' x ',  imgUsed.shape[1])
@@ -60,6 +66,7 @@ class uiThreshold:
             print('Min value: ', imgUsed.min(), ' at pixel ',  imgUsed.argmin())
             print('Variance: ', imgUsed.var())
             print('Standard deviation: ', imgUsed.std())
+            """
             
             # Ziskani okna (figure)
             self.fig = matpyplot.figure()
@@ -75,7 +82,7 @@ class uiThreshold:
             self.fig.subplots_adjust(left = 0.1, bottom = 0.25)
             # Vykresli obrazek
     #        self.im0 = self.ax0.imshow(imgUsed)
-            self.im1 = self.ax1.imshow(imgUsed)
+            self.im1 = self.ax1.imshow(self.imgUsed, self.cmap)
             """
             self.im2 = self.ax2.imshow(imgUsed)
             self.im3 = self.ax3.imshow(imgUsed)
@@ -112,8 +119,8 @@ class uiThreshold:
             self.sclose.on_changed(self.updateBinClosing)
             """
             """"""
-            self.smin.on_changed(self.updateImg)
-            self.smax.on_changed(self.updateImg)
+            self.smin.on_changed(self.updateImg2D)
+            self.smax.on_changed(self.updateImg2D)
             """
             self.sopen.on_changed(self.updateImg)
             self.sclose.on_changed(self.updateImg)
@@ -121,24 +128,91 @@ class uiThreshold:
         
         elif(inputDimension == 3):
             
-            print('Sorry! Not yet implemented for 3 dimensions!\nExiting.')
+            # Zakladni informace o obrazcich (+ statisticke)
+            """
+            print('Image dtype: ', imgUsed.dtype)
+            print('Image size: ', imgUsed.size)
+            print('Image shape: ', imgUsed.shape[0], ' x ',  imgUsed.shape[1], ' x ',  imgUsed.shape[2])
+            print('Max value: ', imgUsed.max(), ' at pixel ',  imgUsed.argmax())
+            print('Min value: ', imgUsed.min(), ' at pixel ',  imgUsed.argmin())
+            print('Variance: ', imgUsed.var())
+            print('Standard deviation: ', imgUsed.std())
+            """
+            
+            self.imgUsed = imgUsed
+            self.imgOutput = self.imgUsed
+            
+            #self.imgMin = numpy.min(self.imgUsed)
+            #self.imgMax = numpy.max(self.imgUsed)
+            
+            self.imgShape = list(self.imgUsed.shape)
+            
+            imgShowPlace = numpy.round(self.imgShape[2] / 2).astype(int)
+            self.imgShow = self.imgUsed[:, :, imgShowPlace]
+            
+            self.fig = matpyplot.figure()
+            # Pridani subplotu do okna (do figure)
+            self.ax1 = self.fig.add_subplot(111)
+            
+            # Upraveni subplotu
+            self.fig.subplots_adjust(left = 0.1, bottom = 0.25)
+            # Vykresli obrazek
+    #        self.im0 = self.ax0.imshow(imgUsed)
+            self.im1 = self.ax1.imshow(self.imgShow, self.cmap)
+
+     #       self.fig.colorbar(self.im1)
+    
+            # Zakladni informace o slideru
+            axcolor = 'white' # lightgoldenrodyellow
+            axmin = self.fig.add_axes([0.25, 0.16, 0.495, 0.03], axisbg = axcolor)
+            axmax  = self.fig.add_axes([0.25, 0.12, 0.495, 0.03], axisbg = axcolor)
+            
+            # Vytvoreni slideru
+                # Minimalni pouzita hodnota v obrazku
+            min0 = numpy.amin(self.imgUsed)
+                # Maximalni pouzita hodnota v obrazku
+            max0 = numpy.amax(self.imgUsed)
+                # Vlastni vytvoreni slideru
+                
+            self.smin = Slider(axmin, 'Minimal threshold', min0, max0, valinit = min0)
+            self.smax = Slider(axmax, 'Maximal threshold', min0, max0, valinit = max0)
+            
+            self.smin.on_changed(self.updateImg3D)
+            self.smax.on_changed(self.updateImg3D)
             
         else:
+            
             print('Wrong input.\nDimension of input is not 2 or 3.\nExiting.')
 
     def showPlot(self):
+        
         # Zobrazeni plot (figure)
         matpyplot.show() 
-        return self.newData
+        return self.imgOutput
 
-    def updateImg(self, val):
+    def updateImg2D(self, val):
         
         # Prahovani (smin, smax)
         img1 = self.imgUsed > self.smin.val
-        self.imgChanged = img1# < self.smax.val
+        self.imgChanged = img1 #< self.smax.val
         
         # Predani obrazku k vykresleni
-        self.im1 = self.ax1.imshow(self.imgChanged)
+        self.im1 = self.ax1.imshow(self.imgChanged, self.cmap)
+        # Prekresleni
+        self.fig.canvas.draw()
+        
+    def updateImg3D(self, val):
+        
+        # Prahovani (smin, smax)
+        round = 0
+        for round in range(self.imgShape[2]):
+            im1 = self.imgUsed[:, :, round] > self.smin.val
+            self.imgOutput[:, :, round] = (im1) #< self.smax.val
+        
+        # Predani obrazku k vykresleni
+        self.imgShow = numpy.amax(self.imgOutput, 2) # self.imgOutput[:, :, self.imgShowPlace]
+        self.im1 = self.ax1.imshow(self.imgShow, self.cmap)
+        
         # Prekresleni
         self.fig.canvas.draw()
     
