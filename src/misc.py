79e7c9dcdbd21c3d2dcb4c90f13f1bc75d8704cb
Miroslav Jirik UL504 <mjirik@kky.zcu.cz>
2013-01-28 14:16:09 +0100

    write to file added

---
+++ src/misc.py
@@ -0,0 +1,42 @@
+
+#import sys
+#import os
+
+import logging
+logger = logging.getLogger(__name__)
+
+def obj_from_file(filename = 'annotation.yaml', filetype = 'yaml'):
+    ''' Read object from file '''
+# TODO solution for file extensions
+    f = open(filename, 'r')
+    if filetype == 'yaml':
+        import yaml
+        obj = yaml.load(f)
+    elif filetype == 'pickle':
+        import pickle
+        obj = pickle.load(f)
+    else:
+        logger.error('Unknown filetype')
+    f.close()
+    return obj
+
+
+def obj_to_file(obj, filename = 'annotation.yaml', filetype = 'yaml'):
+    '''Writes annotation in file
+    '''
+    #import json
+    #with open(filename, mode='w') as f:
+    #    json.dump(annotation,f)
+
+    # write to yaml
+
+    f = open(filename, 'w')
+    if filetype == 'yaml':
+        import yaml
+        yaml.dump(obj,f)
+    elif filetype == 'pickle':
+        import pickle
+        pickle.dump(obj,f)
+    else:
+        logger.error('Unknown filetype')
+    f.close
