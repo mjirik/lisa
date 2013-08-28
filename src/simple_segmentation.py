27cff2bf97352e7ad0763d171a236d3d7d2e1fde
Ondřej Duspiva <ondrej@OndrejPC.(none)>
2013-08-28 18:12:56 +0200

    Pracovni verze souboru, zobrazuje paralelne grafiky na kterych je videt snimek po konvoluci a nalezenou oblast patere kterou pak prenasobi jednicky kosti v matici simple_seg

--- src/simple_segmentation.py
+++ src/simple_segmentation.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
-
+#definice konstant
+BONES = 1290
 
 
 # import funkcí z jiného adresáře
@@ -32,15 +33,65 @@ import py3DSeedEditor
 
 
 class SimpleSegmentation:
+    
     def simple_segmentation(self, data3d, voxelsize_mm):
         simple_seg = np.zeros(data3d.shape )
 
-        # ukázka zápisu do dat
-        simple_seg[10:-10, 20:30, 2:5] = 1
+	#definice konvoluční funkce - param a - matice m*n kterou budeme přenásobovat konvoluční maticí, b - Konvoluční maska m*n
+	def konvoluce(a, b):
+		temp_suma = 0	
+		for i in range(a.shape[0]):
+			for j in range(a.shape[1]):			
+				temp_suma += (a[i][j]*b[i][j]) 
+		return temp_suma        
+	
+	# nalezení kostí
+	simple_seg = data3d > BONES 
+	simple_seg[(simple_seg.shape[0]/5)*4:simple_seg.shape[0]] = 0
+	
+	# nalezení páteře
+	KONV_MASK = np.ones([3,3], float)
+	KONV_MASK = KONV_MASK/9.0	
+	operace = 0
+	spine_finder = np.zeros([data3d.shape[0],data3d.shape[1]], float)
+	for k in range(10):
+		for i in range(data3d.shape[0]):
+			for j in range(data3d.shape[1]):
+				temp_matrix = data3d[j:(j+KONV_MASK.shape[1]),i:(i+KONV_MASK.shape[0]), k]
+				temp_matrix = temp_matrix/100
+				operace += 1
+				#print operace				
+				spine_finder[j][i] += konvoluce(temp_matrix, KONV_MASK)
+				"""temp_matrix = data3d[i:(i+KONV_MASK.shape[0]), j:(j+KONV_MASK.shape[1]), k]			
+#tu je problém nějak musíme z temp_matrix 3d udělat temp_metrix_upravena 2D,
+				print type(temp_matrix)
+				print temp_matrix.shape
+			
+				suma_spfi = 0			
 
-
-        return simple_seg
-
+#Zajistit aby temp_metrix měla jen 2rozměry, protože jinak bude a[i][j] pole 
+			
+				print konvoluce(temp_matrix, KONV_MASK)"""
+	spine_finder_upraveno = spine_finder > (105)
+	######
+	spine_finder_upraveno = spine_finder_upraveno*1
+	spine_finder_upraveno += np.ones([spine_finder.shape[0],spine_finder.shape[1]])
+	
+	for id_x in range(simple_seg.shape[0]):
+		for id_y in range(simple_seg.shape[1]):
+			for id_z in range(simple_seg.shape[2]):
+				simple_seg[id_x][id_y][id_z] = simple_seg[id_x][id_y][id_z]*spine_finder_upraveno[id_x][id_y]
+				if (spine_finder_upraveno[id_x][id_y] == 2):
+		
+	import matplotlib.pyplot as plt 	
+	plt.figure(figsize=(16, 5))
+	plt.subplot(121)
+	plt.imshow(spine_finder, cmap = plt.cm.gray)
+	plt.subplot(122)
+	plt.imshow(spine_finder_upraveno, cmap = plt.cm.gray)
+	
+		
+	return simple_seg
 
         
 def main():
