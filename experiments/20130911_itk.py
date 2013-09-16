1d048d1037fb8f51a9e170d323f106bc5602c0be
Miroslav Jiřík <miroslav.jirik@gmail.com>
2013-09-16 18:27:04 +0200

    simple itk example debug

--- experiments/20130911_itk.py
+++ experiments/20130911_itk.py
@@ -17,25 +17,32 @@ data_path = os.path.join(path_to_script, '../sample_data/jatra_5mm/')
 
 
 
+
+print 'im1: 2D dicom image with SimpleITK'
+im1 = sitk.ReadImage(os.path.join(path_to_script,"../sample_data/jatra_5mm/IM-0001-0005.dcm"))
+pimsh = sitk.Show(im1)
 import pdb; pdb.set_trace()
 
-imraw = sitk.ReadImage(os.path.join(path_to_script,"../sample_data/jatra_5mm/IM-0001-0005.dcm"))
-pimsh = sitk.Show(imraw)
-import pdb; pdb.set_trace()
-
-
+#--------------------------------------------
+print 'im2: 3D dicom image readed with our DataReader and visualization with SimpleITK'
 dr = datareader.DataReader()
 data3d, metadata = dr.Get3DData(data_path)
 
-im3 = sitk.GetImageFromArray(data3d)
-sitk.Show(im3)
+im2 = sitk.GetImageFromArray(data3d)
+sitk.Show(im2)
 
+import pdb; pdb.set_trace()
 
+#--------------------------------------------
+print 'im3: 3D dicom image read and visualization with SimpleITK'
 isr = sitk.ImageSeriesReader()
 seriesids = isr.GetGDCMSeriesIDs(data_path)
 print seriesids
 #dcmnames = isr.GetGDCMSeriesFileNames(data_path )
-dcmnames = isr.GetGDCMSeriesFileNames(data_path, seriesids[0]), True, True, True, True
-
+dcmnames = isr.GetGDCMSeriesFileNames(data_path, seriesids[0], True, True, True, True)
 isr.SetFileNames(dcmnames)
+im3 = isr.Execute()
+sitk.Show(im3)
+
+
 import pdb; pdb.set_trace()
