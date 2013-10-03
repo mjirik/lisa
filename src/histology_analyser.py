ea19b9d2261a65219d36da0046acb1d58f06f9dc
Miroslav Jiřík <miroslav.jirik@gmail.com>
2013-10-03 16:48:34 +0200

    code experiments with filters, failing

--- src/histology_analyser.py
+++ src/histology_analyser.py
@@ -1,5 +1,13 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+
+"""
+python src/histology_analyser.py -i ~/data/medical/data_orig/jatra_mikro_data/Nejlepsi_rozliseni_nevycistene -t 6800 -cr 0 -1 100 300 100 300
+
+"""
+
+
+
 import sys
 import os.path
 path_to_script = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +23,7 @@ import misc
 import datareader
 import SimpleITK as sitk
 import seed_editor_qt as seqt
+import scipy.ndimage
 
 class HistologyAnalyser:
     def __init__ (self, data3d, metadata, threshold):
@@ -37,12 +46,32 @@ class HistologyAnalyser:
 
 
         sitk.Show(datathr * 100)
+        bcf = sitk.BinaryClosingByReconstructionImageFilter()
+        print bcf.GetKernelRadius()
+        print 'fg ', bcf.GetForegroundValue()
+        bcf.SetKernelRadius(3)
+        datathr2 = bcf.Execute(datathr)
+        sitk.Show(datathr2 * 200)
+
         import pdb; pdb.set_trace()
-        dataskel = sitk.BinaryThinning(datathr)
+        dataskel = sitk.BinaryThinning(datathr2)
         sitk.Show(dataskel * 100)
         import pdb; pdb.set_trace()
 
-        self.output = sitk.GetArrayFromImage(dataskel)
+        #self.output = sitk.GetArrayFromImage(dataskel)
+
+#   konluce
+        dataskel_nd = sitk.GetArrayFromImage(dataskel)
+        conv_kernel = np.ones([3,3,3], dtype=np.int16)
+        filtered_nd = scipy.ndimage.filters.convolve(dataskel_nd, conv_kernel)
+        filtered_nd = filtered_nd[dataskel_nd==1]
+
+
+        #cf = sitk.ConvolutionImageFilter()
+        #filtered = cf.Execute(dataskel, conv_kernel)
+        filtered = sitk.GetImageFromArray(filtered_nd.astype(np.int16))
+
+        sitk.Show(filtered * 10)
         import pdb; pdb.set_trace()
 
     def preprocessing(self):
