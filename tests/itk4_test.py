bc620e4ff078043ee2cfa9b1ce9adc7d00ddad62
Miroslav Jiřík <miroslav.jirik@gmail.com>
2013-10-15 14:55:34 +0200

    thinning itk, not working, only 2D algorithm

--- tests/itk4_test.py
+++ tests/itk4_test.py
@@ -34,7 +34,7 @@ class PycutTest(unittest.TestCase):
     interactivetTest = False
     #interactivetTest = True
 
-    def generate_data(self, shp=[128,128,128] ):
+    def generate_data(self, shp=[100,101,102] ):
         """ Generating random data with cubic object inside"""
 
         data = np.zeros(shp)
@@ -47,18 +47,71 @@ class PycutTest(unittest.TestCase):
         #x_noisy = x + np.random.normal(0, 0.6, size=x.shape)
         return data
 
-    def test_segmentation_with_boundary_penalties(self):
-        data = generate_data()
-        dataskel_itk = sitk.GetImageFromArray(data)
+    def test_simple_itk(self):
+        import SimpleITK as sitk
+        data = self.generate_data()
+        data_itk = sitk.GetImageFromArray(data)
+        output = sitk.BinaryThinning(data_itk)
 
-        itk.BinaryThinningImageFilter
+
+
+    def test_itk_skeleton_3d(self):
+        """
+        This is not working. ITK has 2D thinning algorithm only. :-(
+        """
+        data = self.generate_data()
+        
+
+        data = data.astype(np.int16)
+
+        #data_itk = sitk.GetImageFromArray(data)
+        #shape = data_itk.GetSize()
+        shape = data.shape#[::-1]
+
+        imT = itk.Image.SS3
+        im = imT.New(Regions=shape)
+        im.Allocate()
+
+        import pdb; pdb.set_trace()
+        for i in range(0,shape[0]):
+            for j in range(0,shape[1]):
+                for k in range(0,shape[2]):
+                    pixel = int(data[i,j,k])
+                    #print type(pixel)
+                    im.SetPixel([i,j,k], pixel)
+
+        #castFilter = itk.CastImageFilter[itk.Image[itk.F,3], itk.Image[itk.SS,3]]
+
+
+
+# skeletonization
+
+        pixelType = itk.SS
+        imageType = itk.Image[pixelType, 3]
+        thinningFilterType = itk.BinaryThinningImageFilter[imageType, imageType]
+        thinningFilter = thinningFilterType.New()
+        thinningFilter.New()
+        thinningFilter.SetInput(im)
+        thinningFilter.Update()
+        skelim = thinningFilter.GetOutput()
+
+
         import pdb; pdb.set_trace()
 
-:
-        pixelType = itk.UC
-        imageType = itk.Image[pixelType, 3]
-        readerType = itk.BinaryThinningImageFilter[imageType]
-        writerType = itk.ImageFileWriter[imageType]
+        dataout = np.zeros(shape, dtype=np.int8)
+        for i in range(0,shape[0]):
+            for j in range(0,shape[1]):
+                for k in range(0,shape[2]):
+                    pixel = skelim.GetPixel([i,j,k])
+                    dataout[i,j,k] = pixel
+
+
+        import pdb; pdb.set_trace()
+        data_itk = sitk.GetImageFromArray(dataout)
+
+        import pdb; pdb.set_trace()
+        writerType = itk.ImageFileWriter[imageType, imageType]
+
         reader = readerType.New()
         writer = writerType.New()
         reader.SetFileName( argv[1] )
