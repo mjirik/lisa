e09ae75f7683c7b1b150ace9d5fe59c4c6395982
Miroslav Jiřík <miroslav.jirik@gmail.com>
2013-10-31 16:23:15 +0100

    vector and write to yaml

--- src/histology_analyser.py
+++ src/histology_analyser.py
@@ -30,6 +30,7 @@ import csv
 import seed_editor_qt as seqt
 import skelet3d
 import segmentation
+import misc
 
 
 GAUSSIAN_SIGMA = 1
@@ -85,10 +86,12 @@ class HistologyAnalyser:
        #         seeds=data3d_skel.astype(np.int8)
        #         )
         #app.exec_()
-        data3d_nodes = skeleton_nodes(data3d_skel, data3d_thr)
-        data3d_nodes_vis = data3d_nodes.copy()
-        data3d_nodes_vis[data3d_nodes_vis == 3] = 2
-        import pdb; pdb.set_trace()
+        skan = SkeletonAnalyser(data3d_skel, volume_data=data3d_thr)
+        data3d_nodes_vis = skan.sklabel.copy()
+# edges
+        data3d_nodes_vis[data3d_nodes_vis > 0 ] = 1
+# nodes and terminals
+        data3d_nodes_vis[data3d_nodes_vis < 0 ] = 2
 
         pyed = seqt.QTSeedEditor(
                 data3d,
@@ -96,7 +99,7 @@ class HistologyAnalyser:
                 contours=data3d_thr.astype(np.int8)
                 )
         app.exec_()
-        stats = skeleton_analysis(data3d_nodes, data3d_thr)
+        stats = skan.skeleton_analysis()
         #data3d_nodes[data3d_nodes==3] = 2
         self.stats = stats
 
@@ -131,11 +134,12 @@ class HistologyAnalyser:
 
 
         
+    def writeStatsToYAML(self, filename='hist_stats.yaml'):
+        misc.obj_to_file(self.stats, filetype='yaml')
 
         #sitk.
     def writeStatsToCSV(self, filename='hist_stats.csv'):
         data = self.stats
-        import pdb; pdb.set_trace()
 
         with open(filename, 'wb') as csvfile:
             writer = csv.writer(
@@ -173,220 +177,278 @@ class HistologyAnalyser:
         app.exec_()
 
 
-def skeleton_nodes(data3d_skel, data3d_thr):
-    """
-    Return 3d ndarray where 0 is background, 1 is skeleton, 2 is node 
-    and 3 is terminal node
-    
-    """
-    # @TODO  remove data3d_thr
+def curve_model(t, params):
+    p0 = params['start'][0] + t*params['vector'][0]
+    p1 = params['start'][1] + t*params['vector'][1]
+    p2 = params['start'][2] + t*params['vector'][2]
+    return [p0, p1, p2]
 
+
+class SkeletonAnalyser:
+    """
+    Example:
+    skan = SkeletonAnalyser(data3d_skel, volume_data, voxelsize_mm)
+    stats = skan.skeleton_analysis()
+    """
+
+    def __init__(self, data3d_skel, volume_data=None, voxelsize_mm=[1,1,1]):
+        self.volume_data = volume_data
+        self.voxelsize_mm = voxelsize_mm
+        # get array with 1 for edge, 2 is node and 3 is terminal
+        skelet_nodes = self.__skeleton_nodes(data3d_skel, self.volume_data)
+        self.__generate_sklabel(skelet_nodes)
+
+
+    def skeleton_analysis(self):
+        """
+        Glossary:
+        element: line structure of skeleton connected to node on both ends
+        node: connection point of elements. It is one or few voxelsize_mm
+        terminal: terminal node
+        
+
+        """
+        if self.volume_data is not None:
+            skdst = self.__radius_analysis_init()
+
+        stats = {}
+        len_edg = np.max(self.sklabel)
+        len_edg = 30
+        
+        for edg_number in range (1,len_edg):
+            edgst = self.__connection_analysis(edg_number)
+            edgst.update(self.__edge_length(edg_number))
+            edgst.update(self.__edge_curve(edg_number, edgst))
+            #edgst = edge_analysis(sklabel, i)
+            if self.volume_data is not None:
+                edgst['radius'] = float(self.__radius_analysis(skdst, edg_number))
+            stats[edgst['id']] = edgst
+
+
+        return stats
+        #import pdb; pdb.set_trace()
+
+    def __generate_sklabel(self, skelet_nodes):
+
+        sklabel_edg, len_edg = scipy.ndimage.label(skelet_nodes == 1, structure=np.ones([3,3,3]))
+        sklabel_nod, len_nod = scipy.ndimage.label(skelet_nodes > 1, structure=np.ones([3,3,3]))
+
+        self.sklabel = sklabel_edg - sklabel_nod
+
+
+    def __angles(self, edg_number, edg_stats):
+        """
+        run after __edge_curve()
+        """
+# this edge
+        curve_params = edg_stats['curve_params']
+        vector0 = self.get_vector_from_curve(0, 0.25, curve_params)
+        vector1 = self.get_vector_from_curve(1, 0.75, curve_params)
+
+
+        pass
+
+    def __get_vector_from_curve(t0, t1, curve_params):
+        return curve_model(t1, curve_params) - \
+                curve_model(t0,curve_params)
+
+    def __skeleton_nodes(self, data3d_skel, data3d_thr):
+        """
+        Return 3d ndarray where 0 is background, 1 is skeleton, 2 is node 
+        and 3 is terminal node
+        
+        """
+        # @TODO  remove data3d_thr
+
+    
 # -----------------  get nodes --------------------------
-    kernel = np.ones([3,3,3])
-    #kernel[0,0,0]=0
-    #kernel[0,0,2]=0
-    #kernel[0,2,0]=0
-    #kernel[0,2,2]=0
-    #kernel[2,0,0]=0
-    #kernel[2,0,2]=0
-    #kernel[2,2,0]=0
-    #kernel[2,2,2]=0
+        kernel = np.ones([3,3,3])
+        #kernel[0,0,0]=0
+        #kernel[0,0,2]=0
+        #kernel[0,2,0]=0
+        #kernel[0,2,2]=0
+        #kernel[2,0,0]=0
+        #kernel[2,0,2]=0
+        #kernel[2,2,0]=0
+        #kernel[2,2,2]=0
 
-    #kernel = np.zeros([3,3,3])
-    #kernel[1,1,:] = 1
-    #kernel[1,:,1] = 1
-    #kernel[:,1,1] = 1
+        #kernel = np.zeros([3,3,3])
+        #kernel[1,1,:] = 1
+        #kernel[1,:,1] = 1
+        #kernel[:,1,1] = 1
 
-    #data3d_skel = np.zeros([40,40,40])
-    #data3d_skel[10,10,10] = 1
-    #data3d_skel[10,11,10] = 1
-    #data3d_skel[10,12,10] = 1
-    #data3d_skel = data3d_skel.astype(np.int8)
+        #data3d_skel = np.zeros([40,40,40])
+        #data3d_skel[10,10,10] = 1
+        #data3d_skel[10,11,10] = 1
+        #data3d_skel[10,12,10] = 1
+        #data3d_skel = data3d_skel.astype(np.int8)
 
 
-    mocnost = scipy.ndimage.filters.convolve(data3d_skel, kernel) * data3d_skel
-    #import pdb; pdb.set_trace()
+        mocnost = scipy.ndimage.filters.convolve(data3d_skel, kernel) * data3d_skel
+        #import pdb; pdb.set_trace()
 
-    nodes = (mocnost > 3).astype(np.int8)
-    terminals = ((mocnost == 2) | (mocnost == 1)).astype(np.int8)
+        nodes = (mocnost > 3).astype(np.int8)
+        terminals = ((mocnost == 2) | (mocnost == 1)).astype(np.int8)
 
-    nt = nodes - terminals
+        nt = nodes - terminals
 
-    #pyed = seqt.QTSeedEditor(
-    #        mocnost, 
-    #        contours=data3d_thr.astype(np.int8),
-    #        seeds=nt
-    #        )
+        #pyed = seqt.QTSeedEditor(
+        #        mocnost, 
+        #        contours=data3d_thr.astype(np.int8),
+        #        seeds=nt
+        #        )
 
-    #import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
 
-    data3d_skel[nodes==1] = 2
-    data3d_skel[terminals==1] = 3
+        data3d_skel[nodes==1] = 2
+        data3d_skel[terminals==1] = 3
 
-    return data3d_skel
-    
-def node_analysis(sklabel):
-    pass
+        return data3d_skel
+        
+    def node_analysis(sklabel):
+        pass
 
-def element_neighbors(sklabel, el_number):
-    """
-    Gives array of element neghbors numbers
-    """
+    def __element_neighbors(self, el_number):
+        """
+        Gives array of element neghbors numbers
+        """
 
-    BOUNDARY_PX = 5
-    element = (sklabel == el_number)
-    enz = element.nonzero()
+        BOUNDARY_PX = 5
+        element = (self.sklabel == el_number)
+        enz = element.nonzero()
 
 
 
 # limits for square neighborhood
-    lo0 = np.max([0, np.min(enz[0]) - BOUNDARY_PX])
-    lo1 = np.max([0, np.min(enz[1]) - BOUNDARY_PX])
-    lo2 = np.max([0, np.min(enz[2]) - BOUNDARY_PX])
-    hi0 = np.min([sklabel.shape[0], np.max(enz[0]) + BOUNDARY_PX])
-    hi1 = np.min([sklabel.shape[1], np.max(enz[1]) + BOUNDARY_PX])
-    hi2 = np.min([sklabel.shape[2], np.max(enz[2]) + BOUNDARY_PX])
+        lo0 = np.max([0, np.min(enz[0]) - BOUNDARY_PX])
+        lo1 = np.max([0, np.min(enz[1]) - BOUNDARY_PX])
+        lo2 = np.max([0, np.min(enz[2]) - BOUNDARY_PX])
+        hi0 = np.min([self.sklabel.shape[0], np.max(enz[0]) + BOUNDARY_PX])
+        hi1 = np.min([self.sklabel.shape[1], np.max(enz[1]) + BOUNDARY_PX])
+        hi2 = np.min([self.sklabel.shape[2], np.max(enz[2]) + BOUNDARY_PX])
 
 # sklabel crop
-    sklabelcr = sklabel[
-            lo0:hi0,
-            lo1:hi1,
-            lo2:hi2
-            ]
+        sklabelcr = self.sklabel[
+                lo0:hi0,
+                lo1:hi1,
+                lo2:hi2
+                ]
 
-    # element crop
-    element = (sklabelcr == el_number)
+        # element crop
+        element = (sklabelcr == el_number)
 
-    dilat_element = scipy.ndimage.morphology.binary_dilation(
-            element,
-            structure=np.ones([3,3,3])
-            )
+        dilat_element = scipy.ndimage.morphology.binary_dilation(
+                element,
+                structure=np.ones([3,3,3])
+                )
 
-    neighborhood = sklabelcr * dilat_element
+        neighborhood = sklabelcr * dilat_element
 
 # if el_number is edge, return nodes 
-    neighbors = np.unique(neighborhood)
-    neighbors = neighbors [neighbors != 0]
-    neighbors = neighbors [neighbors != el_number]
-    #neighbors = [np.unique(neighborhood) != el_number]
-    #if el_number > 0:
-    #    neighbors = np.unique(neighborhood)[np.unique(neighborhood)<0]
-    #else:
-    #    neighbors = np.unique(neighborhood)[np.unique(neighborhood)>0]
-    return neighbors
+        neighbors = np.unique(neighborhood)
+        neighbors = neighbors [neighbors != 0]
+        neighbors = neighbors [neighbors != el_number]
+        #neighbors = [np.unique(neighborhood) != el_number]
+        #if el_number > 0:
+        #    neighbors = np.unique(neighborhood)[np.unique(neighborhood)<0]
+        #else:
+        #    neighbors = np.unique(neighborhood)[np.unique(neighborhood)>0]
+        return neighbors
 
 
-def __edge_length(sklabel, edg_number, voxelsize_mm):
-    return {'lengthEstimation':np.sum(sklabel == edg_number) + 2}
+    def __edge_length(self, edg_number):
+        #self.voxelsize_mm
+        return {'lengthEstimation':float(np.sum(self.sklabel == edg_number) + 2)}
 
-def __edge_curve(sklabel, edg_number, voxelsize_mm, edgst):
-    pass
+    def __edge_curve(self,  edg_number, edg_stats):
+        retval = {}
+        try:
+            nd00, nd01, nd02 = (edg_stats['nodeId0'] == self.sklabel).nonzero()
+            nd10, nd11, nd12 = (edg_stats['nodeId1'] == self.sklabel).nonzero()
+            point0 = np.array([np.mean(nd00), np.mean(nd01), np.mean(nd02)])
+            point1 = np.array([np.mean(nd10), np.mean(nd11), np.mean(nd12)])
+            retval = {'curve_params':
+                    {'start':point0.tolist(), 
+                        'vector':(point1-point0).tolist()}}
+        except Exception as ex:
+            logger.warning("Problem in __edge_curve()")
+
+        return retval
 
 
-def edge_analysis(sklabel, edg_number):
-    print 'element_analysis'
+    def edge_analysis(sklabel, edg_number):
+        print 'element_analysis'
 
 
-    
+        
 
 
 # element dilate * sklabel[sklabel < 0]
 
-    pass
+        pass
 
-def __radius_analysis_init(sklabel, volume_data, voxelsize_mm=[1,1,1]):
-    """
-    Computes skeleton with distances from edge of volume.
-    sklabel: skeleton or labeled skeleton
-    volume_data: volumetric data with zeros and ones
-    """
-    uq = np.unique(volume_data)
-    if (uq[0]==0) & (uq[1]==1):
-        dst = scipy.ndimage.morphology.distance_transform_edt(
-                volume_data,
-                sampling=voxelsize_mm
-                )
-        dst = dst * (sklabel != 0)
-        
-        return dst
+    def __radius_analysis_init(self):
+        """
+        Computes skeleton with distances from edge of volume.
+        sklabel: skeleton or labeled skeleton
+        volume_data: volumetric data with zeros and ones
+        """
+        uq = np.unique(self.volume_data)
+        if (uq[0]==0) & (uq[1]==1):
+            dst = scipy.ndimage.morphology.distance_transform_edt(
+                    self.volume_data,
+                    sampling=self.voxelsize_mm
+                    )
+            dst = dst * (self.sklabel != 0)
+            
+            return dst
 
-    else:
-        print "__radius_analysis_init() error.  "
-        return None
-
-
-def __radius_analysis(sklabel, skdst, edg_number):
-    """
-    return smaller radius of tube
-    """
-    edg_skdst = skdst * (sklabel == edg_number)
-    return np.mean(edg_skdst[edg_skdst != 0])
+        else:
+            print "__radius_analysis_init() error.  "
+            return None
 
 
+    def __radius_analysis(self, edg_number, skdst):
+        """
+        return smaller radius of tube
+        """
+        edg_skdst = skdst * (self.sklabel == edg_number)
+        return np.mean(edg_skdst[edg_skdst != 0])
 
 
-def __connection_analysis(sklabel, edg_number):
-    """
-    Analysis of which edge is connected
-    """
-    edg_neigh = element_neighbors(sklabel,edg_number)
-    if len(edg_neigh) != 2:
-        print ('Wrong number (' + str(edg_neigh) +
-                ') of connected edges in connection_analysis() for\
-                edge number ' + str(edg_number))
-        edg_stats = {
-                'id':edg_number
-                }
-    else:
-        connectedEdges0 = element_neighbors(sklabel, edg_neigh[0])
-        connectedEdges1 = element_neighbors(sklabel, edg_neigh[1])
+
+
+    def __connection_analysis(self, edg_number):
+        """
+        Analysis of which edge is connected
+        """
+        edg_neigh = self.__element_neighbors(edg_number)
+        if len(edg_neigh) != 2:
+            print ('Wrong number (' + str(edg_neigh) +
+                    ') of connected edges in connection_analysis() for\
+                    edge number ' + str(edg_number))
+            edg_stats = {
+                    'id':edg_number
+                    }
+        else:
+            connectedEdges0 = self.__element_neighbors(edg_neigh[0])
+            connectedEdges1 = self.__element_neighbors(edg_neigh[1])
 # remove edg_number from connectedEdges list
-        connectedEdges0 = connectedEdges0[connectedEdges0 != edg_number]
-        connectedEdges1 = connectedEdges1[connectedEdges1 != edg_number]
-        print 'edg_neigh ', edg_neigh, ' ,0: ', connectedEdges0, '  ,0 '
+            connectedEdges0 = connectedEdges0[connectedEdges0 != edg_number]
+            connectedEdges1 = connectedEdges1[connectedEdges1 != edg_number]
+            print 'edg_neigh ', edg_neigh, ' ,0: ', connectedEdges0, '  ,0 '
 
-        #import pdb; pdb.set_trace()
+            #import pdb; pdb.set_trace()
 
-        edg_stats = {
-                'id':edg_number,
-                'nodeId0':edg_neigh[0],
-                'nodeId1':edg_neigh[1],
-                'connectedEdges0':connectedEdges0,
-                'connectedEdges1':connectedEdges1
-                }
+            edg_stats = {
+                    'id':edg_number,
+                    'nodeId0':int(edg_neigh[0]),
+                    'nodeId1':int(edg_neigh[1]),
+                    'connectedEdges0':connectedEdges0.tolist(),
+                    'connectedEdges1':connectedEdges1.tolist()
+                    }
 
-    return edg_stats
-
-def skeleton_analysis(skelet_nodes, volume_data = None, voxelsize_mm=[1,1,1]):
-    """
-    Glossary:
-    element: line structure of skeleton connected to node on both ends
-    node: connection point of elements. It is one or few voxelsize_mm
-    terminal: terminal node
-    
-
-    """
-    sklabel_edg, len_edg = scipy.ndimage.label(skelet_nodes == 1, structure=np.ones([3,3,3]))
-    sklabel_nod, len_nod = scipy.ndimage.label(skelet_nodes > 1, structure=np.ones([3,3,3]))
-
-    sklabel = sklabel_edg - sklabel_nod
-    if volume_data is not None:
-        skdst = __radius_analysis_init(sklabel, volume_data, voxelsize_mm)
-
-    stats = {}
-    len_edg = 50
-    
-    for edg_number in range (1,len_edg):
-        edgst = __connection_analysis(sklabel, edg_number)
-        edgst.update(__edge_length(sklabel, edg_number, voxelsize_mm))
-        #edgst = edge_analysis(sklabel, i)
-        if volume_data is not None:
-            edgst['radius'] = __radius_analysis(sklabel, skdst, edg_number)
-        stats[edgst['id']] = edgst
-
-
-    return stats
-    #import pdb; pdb.set_trace()
+        return edg_stats
 
 
 
@@ -430,6 +492,8 @@ if __name__ == "__main__":
     ha = HistologyAnalyser(data3d, metadata, args.threshold)
     ha.run()
     ha.writeStatsToCSV()
+    import pdb; pdb.set_trace()
+    ha.writeStatsToYAML()
     #ha.show()
     
 
