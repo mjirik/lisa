7abe8715c6e6a2dd4a8e8dbc621e91a889ba8fe4
Miroslav Jirik <miroslav.jirik@gmail.com>
2013-12-10 14:37:36 +0100

    pep8 debug

--- src/config.py
+++ src/config.py
@@ -20,10 +20,12 @@ import inspect
 
 import misc
 
+
 def get_config(filename, default_cfg):
     """
-    Looks config file and update default_cfg values. If file does not exist 
-    it is created.
+    Looks config file and update default_cfg values.
+
+    If file does not exist it is created.
     """
     if os.path.isfile(filename):
         cfg = misc.obj_from_file(filename, filetype='yaml')
@@ -36,24 +38,26 @@ def get_config(filename, default_cfg):
         # default config
     return cfg_out
 
+
 def get_function_keys(p_fcn):
     #fcn_cfg = {p_fcn.__name__:inspect.getargspec(p_fcn)}
     fcn_cfg = inspect.getargspec(p_fcn)
     return fcn_cfg[0]
 
+
 def get_default_function_config(p_fcn):
     """
-    Return dictionary with keys and default params of function
+    Return dictionary with keys and default params of function.
     """
     #fcn_cfg = {p_fcn.__name__:inspect.getargspec(p_fcn)}
     fcn_argspec = inspect.getargspec(p_fcn)
     valid_keys = fcn_argspec[0][-len(fcn_argspec[3]):]
     fcn_cfg = {}
-    for i in range(0,len(valid_keys)):
-        fcn_cfg[valid_keys[i]]=fcn_argspec[3][i]
+    for i in range(0, len(valid_keys)):
+        fcn_cfg[valid_keys[i]] = fcn_argspec[3][i]
 
+    return fcn_cfg
 
-    return fcn_cfg 
 
 def subdict(bigdict, wanted_keys):
     ret = dict([(i, bigdict[i]) for i in wanted_keys if i in bigdict])
