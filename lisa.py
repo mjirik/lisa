e97ba77bd88a7ebd4cd299c3db637d171fc08207
Miroslav Jiřík <miroslav.jirik@gmail.com>
2014-01-14 13:30:11 +0100

    run script for lisa

---
+++ lisa.py
@@ -0,0 +1,16 @@
+#! /opt/local/bin/python
+# -*- coding: utf-8 -*-
+
+""" Run Lisa. """
+
+# import funkcí z jiného adresáře
+import sys
+import os.path
+
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(path_to_script, "./src"))
+
+if __name__ == "__main__":
+    import organ_segmentation_gui as osegg
+    osegg.main()
+    exit()
