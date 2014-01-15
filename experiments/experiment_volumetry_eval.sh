bcdae3b974b0362c08446dd6dfda6a3c2aa645c7
Miroslav Jiřík <miroslav.jirik@gmail.com>
2014-01-15 09:23:35 +0100

    experiment with 5 data from sliver

---
+++ experiments/experiment_volumetry_eval.sh
@@ -0,0 +1,5 @@
+python lisa.py -dd ~/data/medical/orig/sliver07/training-part1/liver-orig001.mhd -d --roi [[37,180],[36,410],[25,403]]
+python lisa.py -dd ~/data/medical/orig/sliver07/training-part1/liver-orig002.mhd -d --roi [[0,64],[49,471],[11,444]]
+python lisa.py -dd ~/data/medical/orig/sliver07/training-part1/liver-orig003.mhd -d --roi [[4,78],[36,373],[27,366]]
+python lisa.py -dd ~/data/medical/orig/sliver07/training-part1/liver-orig004.mhd -d --roi [[37,208],[78,443],[31,385]]
+python lisa.py -dd ~/data/medical/orig/sliver07/training-part1/liver-orig005.mhd -d --roi [[85,302],[48,470],[0,305]]
