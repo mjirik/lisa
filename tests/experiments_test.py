d6aeba82acc7edc575850b8a5b9b4c69625ef8e4
Miroslav Jiřík <miroslav.jirik@gmail.com>
2014-03-10 10:25:25 +0100

    volumetry evaluation test in progress

--- tests/experiments_test.py
+++ tests/experiments_test.py
@@ -12,6 +12,7 @@ path_to_script = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(path_to_script, "../extern/pyseg_base/src/"))
 sys.path.append(os.path.join(path_to_script, "../extern/py3DSeedEditor/"))
 sys.path.append(os.path.join(path_to_script, "../src/"))
+sys.path.append(os.path.join(path_to_script, "../experiments/"))
 import unittest
 
 import numpy as np
@@ -33,7 +34,19 @@ class ExperimentsTest(unittest.TestCase):
         self.assertTrue('src' in dirlist)
         self.assertFalse('README.md' in dirlist)
 
+    def test_eval_sliver_volume(self):
+        import volumetry_evaluation as ve
 
+        vol1 = np.zeros([20,21,22], dtype=np.int8)
+        vol1 [10:15, 10:15, 10:15] = 1
+
+        vol2 = np.zeros([20,21,22], dtype=np.int8)
+        vol2 [10:15, 10:16, 10:15] = 1
+
+        eval1 = ve.compare_volumes(vol1, vol2, [1, 1, 1])
+        print eval1
+
+        pass
 
 
 
