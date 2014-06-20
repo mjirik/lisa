60dab3b552da5b94c3f17d73b603727cdcd099af
Miroslav Jirik <miroslav.jirik@gmail.com>
2014-06-20 09:25:04 +0200

    fix path to hist_stats_new.yaml

--- tests/histology_test.py
+++ tests/histology_test.py
@@ -33,7 +33,7 @@ class HistologyTest(unittest.TestCase):
         from histology_report import HistologyReport
         import segmentation
         import misc
-        
+
         # generate 3d data from yaml for testing
         tvg = TreeVolumeGenerator()
         yaml_path = os.path.join(path_to_script, "./hist_stats_test.yaml")
@@ -41,40 +41,40 @@ class HistologyTest(unittest.TestCase):
         tvg.voxelsize_mm = [1,1,1]
         tvg.shape = [100,100,100]
         tvg.generateTree()
-        
+
         # init histology Analyser
         metadata = {'voxelsize_mm': tvg.voxelsize_mm}
         data3d = tvg.data3d*10
         threshold = 2.5
         ha = HistologyAnalyser(data3d, metadata, threshold)
-        
+
         # segmented data
         ha.data_to_binar()
         ha.data_to_skeleton()
-        
+
         # get statistics
         ha.data_to_statistics()
-        yaml_new = "hist_stats_new.yaml"
+        yaml_new = os.path.join(path_to_script, "hist_stats_new.yaml")
         ha.writeStatsToYAML(filename=yaml_new)
-        
+
         # get histology reports
         hr = HistologyReport()
         hr.importFromYaml(yaml_path)
         hr.generateStats()
         stats_orig = hr.stats['Report']
-        
+
         hr = HistologyReport()
-        hr.importFromYaml(os.path.join(path_to_script, yaml_new))
+        hr.importFromYaml(yaml_new)
         hr.generateStats()
         stats_new = hr.stats['Report']
-        
+
         # compare
         self.assertGreater(stats_orig['Other']['Total length mm'],stats_new['Other']['Total length mm']*0.9)
         self.assertLess(stats_orig['Other']['Total length mm'],stats_new['Other']['Total length mm']*1.1)
-        
+
         self.assertGreater(stats_orig['Other']['Avg length mm'],stats_new['Other']['Avg length mm']*0.9)
         self.assertLess(stats_orig['Other']['Avg length mm'],stats_new['Other']['Avg length mm']*1.1)
-        
+
         self.assertGreater(stats_orig['Other']['Avg radius mm'],stats_new['Other']['Avg radius mm']*0.9)
         self.assertLess(stats_orig['Other']['Avg radius mm'],stats_new['Other']['Avg radius mm']*1.1)
 
