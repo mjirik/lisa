4e300441e664b0c49632931229a204e98f4a7be9
Miroslav Jirik <miroslav.jirik@gmail.com>
2014-07-03 12:45:12 +0200

    parameters of gmm and unittest

--- src/classification.py
+++ src/classification.py
@@ -4,23 +4,47 @@ import numpy as np
 
 
 class GMMClassifier():
-    def __init__(self):
+
+    def __init__(self, each_class_params=None, **same_params):
+        """
+        same_params: classifier params for each class are same
+        each_class_params: is list of dictionary of params for each
+            class classifier. For example:
+            [{'covariance_type': 'full'}, {'n_components': 2}])
+        """
+        self.same_params = same_params
+        self.each_class_params = each_class_params
 
         self.models = []
 
     def fit(self, X_train, y_train):
         from sklearn.mixture import GMM
-        unlabels = range(0, np.max(y_train)+1)
+
+        unlabels = range(0, np.max(y_train + 1))
+
         for lab in unlabels:
-            model = GMM()
+            if self.each_class_params is not None:
+                # print 'eacl'
+                # print self.each_class_params[lab]
+                model = GMM(**self.each_class_params[lab])
+                # print 'po gmm ', model
+            elif len(self.same_params) > 0:
+                model = GMM(**self.same_params)
+                # print 'ewe ', model
+            else:
+                model = GMM()
             model.fit(X_train[y_train == lab])
+
             self.models.insert(lab, model)
 
     def predict(self, X_test):
         scores = np.zeros([X_test.shape[0], len(self.models)])
+
         for lab in range(0, len(self.models)):
 
             sc = self.models[lab].score(X_test)
             scores[:, lab] = sc
+
         pred = np.argmax(scores, 1)
+
         return pred
---
+++ tests/classification_test.py
@@ -0,0 +1,36 @@
+# ! /usr/bin/python
+# -*- coding: utf-8 -*-
+# import funkcí z jiného adresáře
+import sys
+import os.path
+
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(path_to_script, "../extern/pyseg_base/src/"))
+sys.path.append(os.path.join(path_to_script, "../extern/py3DSeedEditor/"))
+sys.path.append(os.path.join(path_to_script, "../src/"))
+
+import unittest
+import numpy as np
+import classification
+
+
+class OrganSegmentationTest(unittest.TestCase):
+    def test_gmmclassifier(self):
+
+        X_tr = np.array([1, 2, 0, 1, 1, 0, 7, 8, 9, 8, 6, 7]).reshape(-1, 1)
+        y_tr = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]).reshape(-1)
+
+        X_te = np.array([2, 7, 8]).reshape(-1, 1)
+        y_te = np.array([0, 1, 1]).reshape(-1)
+
+        # cl = GMMClassifier(each_class_params=[{},{}])
+        cl = classification.GMMClassifier(each_class_params=[
+            {'covariance_type': 'full'},
+            {'n_components': 2}])
+        cl.fit(X_tr, y_tr)
+
+        self.assertTrue((cl.predict(X_te) == y_te).all())
+
+
+if __name__ == "__main__":
+    unittest.main()
