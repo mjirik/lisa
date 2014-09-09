f6c7b7f2290cb2c0245cb99fdb35e236ddc5b9c8
M. Jirik <miroslav.jirik@gmail.com>
2014-09-09 11:56:50 +0200

    LAR support init

---
+++ src/lar_vessels.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Generator of histology report
+
+"""
+import logging
+logger = logging.getLogger(__name__)
+
+
+class larVessels:
+    def __init__(self):
+        pass
+
+    def add_cylinder(self, nodeA, nodeB, radius):
+        pass
+
+    def show(self):
+        pass
