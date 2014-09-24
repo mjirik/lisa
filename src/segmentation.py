4747ea6dccddc08f98432f764e2a338ca955354d
Miroslav Jirik <miroslav.jirik@gmail.com>
2014-09-24 11:08:18 +0200

    code formating pep8

--- src/segmentation.py
+++ src/segmentation.py
@@ -11,13 +11,12 @@ Copyright:   (c) Pavel Volkovinsky
 -------------------------------------------------------------------------------
 """
 
-import unittest
 import sys
 sys.path.append("../src/")
 sys.path.append("../extern/")
 
 import uiThreshold
-import thresholding_functions
+# import thresholding_functions
 
 import logging
 logger = logging.getLogger(__name__)
@@ -26,22 +25,24 @@ import numpy
 import scipy
 import scipy.ndimage
 
-import gc as garbage
 
-def vesselSegmentation(data, segmentation = -1, threshold = -1, voxelsize_mm = [1,1,1], inputSigma = -1,
-                       dilationIterations = 0, dilationStructure = None, nObj = 10, biggestObjects = False,
-                       useSeedsOfCompactObjects = False,
-                       seeds = None, interactivity = True, binaryClosingIterations = 2,
-                       binaryOpeningIterations = 0, smartInitBinaryOperations = True,
-                       returnThreshold = False, binaryOutput = True, returnUsedData = False):
-
+def vesselSegmentation(data, segmentation=-1, threshold=-1,
+                       voxelsize_mm=[1, 1, 1],
+                       inputSigma=-1, dilationIterations=0,
+                       dilationStructure=None, nObj=10, biggestObjects=False,
+                       useSeedsOfCompactObjects=False, seeds=None,
+                       interactivity=True, binaryClosingIterations=2,
+                       binaryOpeningIterations=0,
+                       smartInitBinaryOperations=True, returnThreshold=False,
+                       binaryOutput=True, returnUsedData=False):
     """
 
     Vessel segmentation z jater.
 
     Input:
         data - CT (nebo MRI) 3D data
-        segmentation - zakladni oblast pro segmentaci, oznacena struktura se stejnymi rozmery jako "data",
+        segmentation - zakladni oblast pro segmentaci, oznacena struktura se
+        stejnymi rozmery jako "data",
             kde je oznaceni (label) jako:
                 1 jatra,
                 -1 zajimava tkan (kosti, ...)
@@ -49,17 +50,24 @@ def vesselSegmentation(data, segmentation = -1, threshold = -1, voxelsize_mm = [
         threshold - prah
         voxelsize_mm - (vektor o hodnote 3) rozmery jednoho voxelu
         inputSigma - pocatecni hodnota pro prahovani
-        dilationIterations - pocet operaci dilation nad zakladni oblasti pro segmentaci ("segmantation")
+        dilationIterations - pocet operaci dilation nad zakladni oblasti pro
+            segmentaci ("segmantation")
         dilationStructure - struktura pro operaci dilation
-        nObj - oznacuje, kolik nejvetsich objektu se ma vyhledat - pokud je rovno 0 (nule), vraci cela data
+        nObj - oznacuje, kolik nejvetsich objektu se ma vyhledat - pokud je
+            rovno 0 (nule), vraci cela data
         biggestObjects - moznost, zda se maji vracet nejvetsi objekty nebo ne
-        seeds - moznost zadat pocatecni body segmentace na vstupu. Je to matice o rozmerech jako data. Vsude nuly, tam kde je oznaceni jsou jednicky.
-        interactivity - nastavi, zda ma nebo nema byt pouzit interaktivni mod upravy dat
+        seeds - moznost zadat pocatecni body segmentace na vstupu. Je to matice
+            o rozmerech jako data. Vsude nuly, tam kde je oznaceni jsou jednicky
+        interactivity - nastavi, zda ma nebo nema byt pouzit interaktivni mod
+            upravy dat
         binaryClosingIterations - vstupni binary closing operations
         binaryOpeningIterations - vstupni binary opening operations
-        smartInitBinaryOperations - logicka hodnota pro smart volbu pocatecnich hodnot binarnich operaci (bin. uzavreni a bin. otevreni)
-        returnThreshold - jako druhy parametr funkce vrati posledni hodnotu prahu
-        binaryOutput - zda ma byt vystup vracen binarne nebo ne (binarnim vystupem se rozumi: cokoliv jineho nez hodnota 0 je hodnota 1)
+        smartInitBinaryOperations - logicka hodnota pro smart volbu pocatecnich
+            hodnot binarnich operaci (bin. uzavreni a bin. otevreni)
+        returnThreshold - jako druhy parametr funkce vrati posledni hodnotu
+            prahu
+        binaryOutput - zda ma byt vystup vracen binarne nebo ne (binarnim
+            vystupem se rozumi: cokoliv jineho nez hodnota 0 je hodnota 1)
         returnUsedData - vrati pouzita data
 
     Output:
@@ -68,96 +76,111 @@ def vesselSegmentation(data, segmentation = -1, threshold = -1, voxelsize_mm = [
     """
 
     dim = numpy.ndim(data)
-    logger.debug( 'Dimenze vstupnich dat: ' + str(dim))
+    logger.debug('Dimenze vstupnich dat: ' + str(dim))
     if (dim < 2) or (dim > 3):
-        logger.debug( 'Nepodporovana dimenze dat!')
-        logger.debug( 'Ukonceni funkce!')
+        logger.debug('Nepodporovana dimenze dat!')
+        logger.debug('Ukonceni funkce!')
         return None
 
     if seeds == None:
-        logger.debug( 'Funkce spustena bez prioritnich objektu!')
+        logger.debug('Funkce spustena bez prioritnich objektu!')
 
     if biggestObjects:
-        logger.debug( 'Funkce spustena s vracenim nejvetsich objektu => nebude mozne vybrat prioritni objekty!')
+        logger.debug(
+            'Funkce spustena s vracenim nejvetsich objektu => nebude mozne\
+vybrat prioritni objekty!')
 
-    if ( nObj < 1 ) :
+    if (nObj < 1):
         nObj = 1
 
     if biggestObjects:
-        logger.debug( 'Vybrano objektu k vraceni: ' + str(nObj))
+        logger.debug('Vybrano objektu k vraceni: ' + str(nObj))
 
     logger.debug('Pripravuji data...')
 
     voxel = numpy.array(voxelsize_mm)
 
-    ## Kalkulace objemove jednotky (voxel) (V = a*b*c).
-    voxel1 = voxel[0]#[0]
-    voxel2 = voxel[1]#[0]
-    voxel3 = voxel[2]#[0]
+    # Kalkulace objemove jednotky (voxel) (V = a*b*c).
+    voxel1 = voxel[0]  # [0]
+    voxel2 = voxel[1]  # [0]
+    voxel3 = voxel[2]  # [0]
     voxelV = voxel1 * voxel2 * voxel3
 
-    ## number je zaokrohleny 2x nasobek objemove jednotky na 2 desetinna mista.
-    ## number stanovi doporucenou horni hranici parametru gauss. filtru.
-    number = (numpy.round((2 * voxelV**(1.0/3.0)), 2))
+    # number je zaokrohleny 2x nasobek objemove jednotky na 2 desetinna mista.
+    # number stanovi doporucenou horni hranici parametru gauss. filtru.
+    number = (numpy.round((2 * voxelV ** (1.0 / 3.0)), 2))
 
-    ## Operace dilatace (dilation) nad samotnymi jatry ("segmentation").
+    # Operace dilatace (dilation) nad samotnymi jatry ("segmentation").
     if(dilationIterations > 0.0):
-        segmentation = scipy.ndimage.binary_dilation(input = segmentation,
-            structure = dilationStructure, iterations = dilationIterations)
+        segmentation = scipy.ndimage.binary_dilation(
+            input=segmentation, structure=dilationStructure,
+            iterations=dilationIterations)
 
-    ## Ziskani datove oblasti jater (bud pouze jater nebo i jejich okoli - zalezi,
-    ## jakym zpusobem bylo nalozeno s operaci dilatace dat).
-    preparedData = (data * (segmentation == 1))#.astype(numpy.float)
-    logger.debug( 'Typ vstupnich dat: ' + str(preparedData.dtype))
+    # Ziskani datove oblasti jater (bud pouze jater nebo i jejich okoli -
+    # zalezi, jakym zpusobem bylo nalozeno s operaci dilatace dat).
+    preparedData = (data * (segmentation == 1))  # .astype(numpy.float)
+    logger.debug('Typ vstupnich dat: ' + str(preparedData.dtype))
 
 #    if preparedData.dtype != numpy.uint8:
 #        print 'Data nejsou typu numpy.uint8 => muze dojit k errorum'
 
     if not numpy.can_cast(preparedData.dtype, numpy.float):
-       logger.debug( 'ERROR: (debug message) Data nejsou takoveho typu, aby se daly prevest na typ "numpy.float" => muze dojit k errorum')
-       logger.debug( 'Ukoncuji funkci!')
-       return None
+        logger.debug(
+            'ERROR: (debug message) Data nejsou takoveho typu, aby se daly \
+prevest na typ "numpy.float" => muze dojit k errorum')
+        logger.debug('Ukoncuji funkci!')
+        return None
 
     if (preparedData == False).all():
-       logger.debug( 'ERROR: (debug message) Jsou spatna data nebo segmentacni matice: all is true == data is all false == bad segmentation matrix (if data matrix is ok)')
-       logger.debug( 'Ukoncuji funkci!')
-       return None
+        logger.debug(
+            'ERROR: (debug message) Jsou spatna data nebo segmentacni matice: \
+all is true == data is all false == bad segmentation matrix (if data matrix is \
+ok)')
+        logger.debug('Ukoncuji funkci!')
+        return None
 
     del(data)
     del(segmentation)
 
-    ## Nastaveni rozmazani a prahovani dat.
+    # Nastaveni rozmazani a prahovani dat.
     if(inputSigma == -1):
         inputSigma = number
     if(inputSigma > number):
         inputSigma = number
 
     seeds = None
-    if biggestObjects == False and seeds == None and interactivity == True and threshold==-1:
+    if biggestObjects == False and\
+            seeds == None and interactivity == True and threshold == -1:
 
-        logger.debug(('Nyni si levym nebo pravym tlacitkem mysi (klepnutim nebo tazenim) oznacte specificke oblasti k vraceni.'))
+        logger.debug(
+            ('Nyni si levym nebo pravym tlacitkem mysi (klepnutim nebo tazenim)\
+ oznacte specificke oblasti k vraceni.'))
         import py3DSeedEditor
         pyed = py3DSeedEditor.py3DSeedEditor(preparedData)
         pyed.show()
         seeds = pyed.seeds
 
-        ## Zkontrolovat, jestli uzivatel neco vybral - nejaky item musi byt ruzny od nuly.
+        # Zkontrolovat, jestli uzivatel neco vybral - nejaky item musi byt
+        # ruzny od nuly.
         if (seeds != 0).any() == False:
 
             seeds = None
-            logger.debug( 'Zadne seedy nezvoleny => nejsou prioritni objekty.')
+            logger.debug('Zadne seedy nezvoleny => nejsou prioritni objekty.')
 
         else:
 
-            seeds = seeds.nonzero()#seeds * (seeds != 0) ## seeds je n-tice poli indexu nenulovych prvku => item krychle je == krychle[ seeds[0][x], seeds[1][x], seeds[2][x] ]
-            logger.debug( 'Seedu bez nul: ' + str(len(seeds[0])))
+            # seeds * (seeds != 0) ## seeds je n-tice poli indexu nenulovych
+            # prvku => item krychle je == krychle[ seeds[0][x], seeds[1][x],
+            # seeds[2][x] ]
+            seeds = seeds.nonzero()
+            logger.debug('Seedu bez nul: ' + str(len(seeds[0])))
 
     closing = binaryClosingIterations
     opening = binaryOpeningIterations
 
     if (smartInitBinaryOperations and interactivity):
 
-        if (seeds == None):
+        if (seeds == None):  # noqa
 
             closing = 5
             opening = 1
@@ -167,26 +190,26 @@ def vesselSegmentation(data, segmentation = -1, threshold = -1, voxelsize_mm = [
             closing = 2
             opening = 0
 
-    ## Samotne filtrovani.
+    # Samotne filtrovani.
     uiT = uiThreshold.uiThreshold(
         preparedData, voxel=voxel, threshold=threshold,
         interactivity=interactivity, number=number, inputSigma=inputSigma,
-        nObj=nObj, biggestObjects=biggestObjects, 
+        nObj=nObj, biggestObjects=biggestObjects,
         useSeedsOfCompactObjects=useSeedsOfCompactObjects,
         binaryClosingIterations=closing, binaryOpeningIterations=opening,
         seeds=seeds)
     output = uiT.run()
 
-    ## Vypocet binarni matice.
-    if output == None:
+    # Vypocet binarni matice.
+    if output == None:  # noqa
 
-        logger.debug( 'Zadna data k vraceni! (output == None)')
+        logger.debug('Zadna data k vraceni! (output == None)')
 
     elif binaryOutput:
 
         output[output != 0] = 1
 
-    ## Vraceni matice.
+    # Vraceni matice.
     if returnThreshold:
 
         if returnUsedData:
@@ -206,4 +229,3 @@ def vesselSegmentation(data, segmentation = -1, threshold = -1, voxelsize_mm = [
         else:
 
             return output
-
