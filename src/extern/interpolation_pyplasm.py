0d26a41541e7c5c547386b502ad51b202068930f
M. Jirik <miroslav.jirik@gmail.com>
2014-09-24 16:42:32 +0200

    lar interpolation functions

---
+++ src/extern/interpolation_pyplasm.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+from larcc import *
+from lar2psm import *
+from mapper import *
+from splines import *
+
+
+def TRIANGULAR_COONS_PATCH(params):
+    ab0Fn, bc1Fn, ca0Fn = params
+
+    def TRIANGULAR_COONS_PATCH0(point):
+        u, v, w = points
+
+        if(hasattr(ab0Fn, '__call__')):
+            Sab0 = ab0Fn(point)
+        else:
+            Sab0 = ab0Fn
+        if(hasattr(bc1Fn, '__call__')):
+            Sbc1 = bc1Fn(point)
+        else:
+            Sbc1 = bc1Fn
+        if(hasattr(ca0Fn, '__call__')):
+            Sca0 = ca0Fn(point)
+        else:
+            Sca0 = ca0Fn
+
+        rn = len(Sab0)
+        mapped = [0] * rn
+
+        if (u == 1):
+            ur = 0
+        else:
+            ur = u * v / (v + w)
+        if (v == 1):
+            vs = 0
+        else:
+            vs = v * w / (w + u)
+        if (w == 1):
+            wt = 0
+        else:
+            wt = w * u / (v + u)
+
+        for i in range(0, rn):
+            mapped[i] = u * Sab0[i] + ur * (Sca0[i] - Sab0[i]) + v * Sca0[
+                i] + vs * (Sbc1[i] - Sca0[i]) + w * Sbc1[i] + wt * (Sab0[i] - Sbc1[i])
+        return mapped
+    return TRIANGULAR_COONS_PATCH0
+
+
+def TRIANGLE_DOMAIN(n, points):
+    pa, pb, pc = points
+    net = []
+    cells = []
+
+    for i in range(0, n + 1):
+        net.append([pa[0] + i * (pb[0] - pa[0]) / double(n), pa[1] + i *
+                   (pb[1] - pa[1]) / n, pa[2] + i * (pb[2] - pa[2]) / double(n)])
+
+    for y in range(0, n + 1):
+        r0 = (y - 1) * (n + 2) - (y - 1) * y / 2
+        r1 = y * (n + 2) - y * (y + 1) / 2
+        for x in range(0, n + 1):
+            c0 = r0 + x
+            c1 = r1 + x
+            net.append([pa[0] + x * (pb[0] - pa[0]) / double(n) + y * (pc[0] - pa[0]) / double(n), pa[1] + x * (pb[1] - pa[1])
+                       / n + y * (pc[1] - pa[1]) / n, pa[2] + x * (pb[2] - pa[2]) / double(n) + y * (pc[2] - pa[2]) / double(n)])
+            if (x > 0):
+                cells.append([c1, c0, c1 - 1])
+                cells.append([c1, c0 + 1, c0])
+
+        print net[:10]
+        print cells[:10]
+    return MKPOLS([net, cells])
---
+++ tests/interpolation_plasm_test.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(path_to_script, "../src/extern"))
+sys.path.append(os.path.join(path_to_script, "../../lar-cc/lib/py/"))
+import unittest
+
+# import numpy as np
+
+
+class InterpolationPlasmTest(unittest.TestCase):
+    skipInteractiveTests = True
+    skipInteractiveTests = False
+    # interactivetTest = True
+
+    def test_store_to_SparseMatrix_and_back(self):
+        """
+        Test has no assert part. Its passed if any funcion does throw exception.
+        """
+        import interpolation_pyplasm as ip
+
+        dom2D = ip.TRIANGLE_DOMAIN(5, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+    @unittest.skipIf(skipInteractiveTests, "test is with visualization")
+    def test_complex_sample(self):
+        from larcc import *
+        from splines import *
+        import interpolation_pyplasm as ip
+
+        # DRAW = COMP([VIEW, STRUCT, MKPOLS])
+        # print 'S1 bezier ', S1
+        # S0 = SEL(0)
+        BEZIER(S2)
+        dom1D = INTERVALS(1)(32)
+        dom2D = ip.TRIANGLE_DOMAIN(32, [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        print dom2D
+        Cab0 = BEZIER(S1)([[10, 0, 0], [6, 0, 3], [3, 0, 3], [0, 0, 0]])
+        VIEW(MAP(Cab0)(dom1D))
+        Cbc0 = BEZIER(S1)(
+            [[10, 0, 0], [10, 2, 4], [8, 8, -4], [2, 10, 4], [0, 10, 0]])
+        Cbc1 = BEZIER(S2)(
+            [[10, 0, 0], [10, 2, 4], [8, 8, -4], [2, 10, 4], [0, 10, 0]])
+        VIEW(MAP(Cbc0)(dom1D))
+        Cca0 = BEZIER(S1)([[0, 10, 0], [0, 6, -5], [0, 3, 5], [0, 0, 0]])
+        VIEW(MAP(Cca0)(dom1D))
+        out = MAP(ip.TRIANGULAR_COONS_PATCH([Cab0, Cbc1, Cca0]))(dom2D)
+        VIEW(out)
+        VIEW(SKELETON(1)(out))
+        # self.assertTrue(np.all(data == data2))
+
+if __name__ == "__main__":
+    unittest.main()
