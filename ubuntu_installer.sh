9120ab4bcb2f842204ff53df09b1302dd73124d2
M. Jirik <miroslav.jirik@gmail.com>
2014-11-10 09:49:26 +0100

    fix that cd is not working with sudo

--- ubuntu_installer.sh
+++ ubuntu_installer.sh
@@ -20,9 +20,11 @@ easy_install -U SimpleITK mahotas
 # 3. pip install our packages pyseg_base and dicom2fem
 sudo -u $USER pip install pysegbase dicom2fem sed3 sed3 io3d --user
 
-# 4. install gco_python
 sudo -u $USER mkdir ~/projects
-sudo -u $USER git clone https://github.com/mjirik/gco_python.git ~/projects
+
+# 4. install gco_python
+sudo -u $USER mkdir ~/projects/gco_python
+sudo -u $USER git clone https://github.com/mjirik/gco_python.git ~/projects/gco_python
 sudo -u $USER make -C ~/projects/gco_python
 sudo -u $USER python ~/projects/gco_python/setup.py --user
 # sudo -u $USER cd ..
@@ -30,17 +32,21 @@ sudo -u $USER python ~/projects/gco_python/setup.py --user
 # 5. skelet3d - optional for Histology Analyser
 sudo apt-get install cmake python-numpy libinsighttoolkit3-dev libpng12-dev
 # sudo -u $USER cd ~/projects
+sudo -u $USER mkdir ~/projects/skelet3d
+sudo -u $USER mkdir ~/projects/skelet3d/build
 sudo -u $USER git clone https://github.com/mjirik/skelet3d.git ~/projects
-sudo -u $USER cd skelet3d
-sudo -u $USER mkdir build
-sudo -u $USER cd build
+cd $HOMEDIR
+cd projects/skelet3d/build
 sudo -u $USER cmake ..
 sudo -u $USER make
 sudo make install
+# sudo -u $USER sh -c "cd ~/projects/skelet3d/build && cmake .. && make"
+# sudo -u $USER mkdir build
+# sudo -u $USER cd build
 
-sudo -u $USER cd ~/projects
+cd ~/projects
 sudo -u $USER git clone --recursive -b stable https://github.com/mjirik/lisa.git
-sudo -u $USER cd lisa
+cd lisa
 sudo -u $USER python mysetup.py -d
 sudo -u $USER python mysetup.py -icn
 
