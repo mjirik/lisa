34b954d6dd6c981106f0722e131bd08ceb39fc6a
M. Jirik <mjirik@kky.zcu.cz>
2014-11-14 19:26:25 +0100

    comment

--- tests/run_this_in_vm.sh
+++ tests/run_this_in_vm.sh
@@ -8,6 +8,7 @@
 
 wget https://raw.githubusercontent.com/mjirik/lisa/master/ubuntu_installer.sh
 chmod a+x ubuntu_installer.sh
+# nothing, devel or something to use https
 echo trustz | sudo -S ./ubuntu_installer.sh lastest
 rm ubuntu_installer.sh
 cd ~/projects/lisa
