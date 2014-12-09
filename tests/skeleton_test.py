37035a264f55084587f26dcddf8c13bfc4312c38
M. Jirik <miroslav.jirik@gmail.com>
2014-12-09 22:18:53 +0100

    tortuoisity test

--- tests/skeleton_test.py
+++ tests/skeleton_test.py
@@ -13,13 +13,40 @@ import numpy as np
 import unittest
 from nose.plugins.attrib import attr
 import lisa.skeleton_analyser as sk
-# import sed3 as ped
 import copy
 
 
 class TemplateTest(unittest.TestCase):
 
     @attr('actual')
+    def test_tortuosity(self):
+        import skelet3d
+
+        data = np.zeros([20, 20, 20], dtype=np.int8)
+        # banana
+        data[5, 3:11, 4:6] = 1
+        data[5, 10:12, 5:12] = 1
+
+        # bar
+        data[5, 3:11, 15:17] = 1
+
+        # import sed3 as ped
+        # pe = ped.sed3(data)
+        # pe.show()
+        skel = skelet3d.skelet3d(data)
+
+        # pe = ped.sed3(skel)
+        # pe.show()
+
+        skan = sk.SkeletonAnalyser(copy.copy(skel), volume_data=data,
+                                   voxelsize_mm=[1, 1, 1])
+        vessel_tree = skan.skeleton_analysis()
+
+        # banana
+        self.assertGreater(vessel_tree[1]['tortuosity'], 1.2)
+        # bar
+        self.assertEqual(vessel_tree[2]['tortuosity'], 1)
+
     def test_fileter_small(self):
         import skelet3d
 
