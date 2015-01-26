17374e1615c8ac2a242ab9e98795b17b61a811e5
M. Jirik <mjirik@kky.zcu.cz>
2015-01-26 13:57:06 +0100

    pep8

--- lisa/histology_report.py
+++ lisa/histology_report.py
@@ -14,7 +14,9 @@ import numpy as np
 import misc
 import csv
 
+
 class HistologyReport:
+
     def __init__(self):
         self.data = None
         self.stats = None
@@ -36,24 +38,23 @@ class HistologyReport:
             data['General']['surface_density'] = None
         return data
 
-
     def writeReportToYAML(self, filename='hist_report'):
         logger.debug('write report to yaml')
-        filename = filename+'.yaml'
+        filename = filename + '.yaml'
         misc.obj_to_file(self.stats, filename=filename, filetype='yaml')
 
     def writeReportToCSV(self, filename='hist_report'):
         logger.debug('write report to csv')
-        filename = filename+'.csv'
+        filename = filename + '.csv'
         data = self.stats['Report']
 
         with open(filename, 'wb') as csvfile:
             writer = csv.writer(
-                    csvfile,
-                    delimiter=';',
-                    quotechar='"',
-                    quoting=csv.QUOTE_MINIMAL
-                    )
+                csvfile,
+                delimiter=';',
+                quotechar='"',
+                quoting=csv.QUOTE_MINIMAL
+            )
             # Main
             writer.writerow([data['Main']['Vessel volume fraction (Vv)']])
             writer.writerow([data['Main']['Surface density (Sv)']])
@@ -69,7 +70,7 @@ class HistologyReport:
             writer.writerow(data['Other']['Length histogram'][0])
             writer.writerow(data['Other']['Length histogram'][1])
 
-    def generateStats(self,binNum=40):
+    def generateStats(self, binNum=40):
         # TODO - upravit dokumentaci
         """
         Funkce na vygenerování statistik.
@@ -78,25 +79,26 @@ class HistologyReport:
         | Avg radius mm: průměrný poloměr jednotlivých segmentů
         | Total length mm: celková délka cév ve vzorku
         | Radius histogram: pole čísel, kde budou data typu:
-        |    v poloměru od 1 do 5 je ve vzorku 12 cév, od 5 do 10 je jich 36, nad 10 je jich 12.
+        |    v poloměru od 1 do 5 je ve vzorku 12 cév, od 5 do 10 je jich 36,
+        |    nad 10 je jich 12.
         |    Využijte třeba funkci np.hist()
         | Length histogram: obdoba předchozího pro délky
         """
         stats = {
-            'Main':{
+            'Main': {
                 'Vessel volume fraction (Vv)': '-',
                 'Surface density (Sv)': '-',
                 'Length density (Lv)': '-',
                 'Tortuosity': '-',
                 'Nv': '-',
-                },
-            'Other':{
+            },
+            'Other': {
                 'Avg length mm': '-',
                 'Total length mm': '-',
                 'Avg radius mm': '-',
                 'Radius histogram': None,
                 'Length histogram': None
-                }
+            }
         }
 
         # Get other stats
@@ -108,28 +110,37 @@ class HistologyReport:
 
         num_of_entries = len(length_array)
         stats['Other']['Total length mm'] = sum(length_array)
-        stats['Other']['Avg length mm'] = stats['Other']['Total length mm']/float(num_of_entries)
-        stats['Other']['Avg radius mm'] = sum(radius_array)/float(num_of_entries)
+        stats['Other']['Avg length mm'] = stats[
+            'Other']['Total length mm'] / float(num_of_entries)
+        stats['Other']['Avg radius mm'] = sum(
+            radius_array) / float(num_of_entries)
 
-        radiusHistogram = np.histogram(radius_array,bins=binNum)
-        stats['Other']['Radius histogram'] = [radiusHistogram[0].tolist(),radiusHistogram[1].tolist()]
-        lengthHistogram = np.histogram(length_array,bins=binNum)
-        stats['Other']['Length histogram'] = [lengthHistogram[0].tolist(),lengthHistogram[1].tolist()]
+        radiusHistogram = np.histogram(radius_array, bins=binNum)
+        stats['Other']['Radius histogram'] = [
+            radiusHistogram[0].tolist(), radiusHistogram[1].tolist()]
+        lengthHistogram = np.histogram(length_array, bins=binNum)
+        stats['Other']['Length histogram'] = [
+            lengthHistogram[0].tolist(), lengthHistogram[1].tolist()]
 
         # get main stats
         tortuosity_array = []
         for key in self.data['Graph']:
             tortuosity_array.append(self.data['Graph'][key]['tortuosity'])
         num_of_entries = len(tortuosity_array)
-        stats['Main']['Tortuosity'] = sum(tortuosity_array)/float(num_of_entries)
-        stats['Main']['Length density (Lv)'] = float(stats['Other']['Total length mm'])/float(self.data['General']['used_volume_mm3'])
-        stats['Main']['Vessel volume fraction (Vv)'] = self.data['General']['vessel_volume_fraction']
-        stats['Main']['Surface density (Sv)'] = self.data['General']['surface_density']
+        stats['Main']['Tortuosity'] = sum(
+            tortuosity_array) / float(num_of_entries)
+        stats['Main']['Length density (Lv)'] = float(
+            stats['Other']['Total length mm']) / float(
+                self.data['General']['used_volume_mm3'])
+        stats['Main']['Vessel volume fraction (Vv)'] = self.data[
+            'General']['vessel_volume_fraction']
+        stats['Main']['Surface density (Sv)'] = self.data[
+            'General']['surface_density']
         stats['Main']['Nv'] = float(self.getNv())
 
         # save stats
-        self.stats = {'Report':stats}
-        logger.debug('Main stats: '+str(stats['Main']))
+        self.stats = {'Report': stats}
+        logger.debug('Main stats: ' + str(stats['Main']))
 
     def getNv(self):
         logger.debug('Computing Nv...')
@@ -140,21 +151,21 @@ class HistologyReport:
                 nodeIdA = edge['nodeIdA']
                 nodeIdB = edge['nodeIdB']
 
-                if not nodeIdA in nodes:
+                if nodeIdA not in nodes:
                     if len(edge['connectedEdgesA']) > 0:
                         nodes.append(nodeIdA)
 
-                if not nodeIdB in nodes:
+                if nodeIdB not in nodes:
                     if len(edge['connectedEdgesB']) > 0:
                         nodes.append(nodeIdB)
 
             except Exception, e:
-                logger.warning('getNv(): bad key '+str(e))
+                logger.warning('getNv(): bad key ' + str(e))
 
-        logger.debug('Got '+str(len(nodes))+' connected nodes')
+        logger.debug('Got ' + str(len(nodes)) + ' connected nodes')
         Nv = len(nodes) / float(self.data['General']['used_volume_mm3'])
 
-        logger.debug('Nv is '+str(Nv))
+        logger.debug('Nv is ' + str(Nv))
         return Nv
 
 
@@ -180,8 +191,8 @@ if __name__ == "__main__":
 
     logger = logging.getLogger()
     logger.setLevel(logging.WARNING)
-    #ch = logging.StreamHandler()
-    #logger.addHandler(ch)
+    # ch = logging.StreamHandler()
+    # logger.addHandler(ch)
 
     if args.debug:
         logger.setLevel(logging.DEBUG)
