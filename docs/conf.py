8eba873989d26a08f5a0bd3c4fd2939e3dd647a4
M. Jirik <mjirik@kky.zcu.cz>
2015-01-27 09:49:14 +0100

    mock of ndimage

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../'))
 import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
-                'scipy.interpolate']
+                'scipy.interpolate', 'scipy.ndimage']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
