4d2f0f22e2017d4538ee210c3a390d23e09a2f45
M. Jirik <mjirik@kky.zcu.cz>
2015-01-27 12:36:07 +0100

    import exter.lar

--- docs/conf.py
+++ docs/conf.py
@@ -29,20 +29,16 @@ MOCK_MODULES = [
     'pysegbase.pycut', 'sklearn', 'skimage', 'dicom', 'vtk', 'vtk.util',
     'larcc', 'larcc.VIEW', 'larcc.MKPOL', 'larcc.AA', 'larcc.INTERVALS',
     'larcc.MAP', 'PyQt4', 'PyQt4.QtCore', 'PyQt4.QtGui', 'web', 'lar2psm',
-    'scipy.ndimage.measurements',  'lar', 'splines', 'scipy.sparse',
-    'skimage.filter', 'mapper', 'skelet3d', 'numpy.core', 'lbpLibrary',
-    'skimage.exposure', 'PyQt4.QVTKRenderWindowInteractor',
+    'scipy.ndimage.measurements',  'lar', 'extern.lar', 'splines',
+    'scipy.sparse', 'skimage.filter', 'mapper', 'skelet3d', 'numpy.core',
+    'lbpLibrary', 'skimage.exposure', 'PyQt4.QVTKRenderWindowInteractor',
     'matplotlib.backends', 'matplotlib.backends.backend_qt4agg', 'numpy.linalg',
     'PyQt4.Qt', 'matplotlib.figure', 'skimage.morphology', 'gtk',
     'pysegbase.seed_editor_qt', 'vtk.qt4', 'vtk.qt4.QVTKRenderWindowInteractor',
     'seg2fem', 'skimage.segmentation', 'matplotlib.patches', 'skimage.feature',
     'scipy.ndimage.morphology', 'mpl_toolkits', 'mpl_toolkits.mplot3d',
-    'matplotlib.backends.backend_gtkagg',
-    # 'mpl_toolkits.mplot3d.art3d',
-    'cv2', 'skimage.measure', 'dicom2fem',
+    'matplotlib.backends.backend_gtkagg', 'cv2', 'skimage.measure', 'dicom2fem',
     'scipy.stats'
-# vtk.qt4.QVTKRenderWindowInteractor
-
 ]
 
 for mod_name in MOCK_MODULES:
