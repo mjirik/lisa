c68add8bface11de2769c0a7e6b4583bc14c81f5
M. Jirik <mjirik@kky.zcu.cz>
2015-01-27 09:57:54 +0100

    sklear mock

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot',
                 'scipy.interpolate', 'scipy.ndimage', 'pycut', 'io3d', 'sed3',
-                'pysegbase', 'pysegbase.pycut']
+                'pysegbase', 'pysegbase.pycut', 'sklearn', 'skimage', 'dicom']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
