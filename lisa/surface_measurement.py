fa494de2e47a3918f26f5eb95c4ac7deb44fa165
M. Jirik <mjirik@kky.zcu.cz>
2015-02-09 13:57:52 +0100

    aoi implementation

--- lisa/surface_measurement.py
+++ lisa/surface_measurement.py
@@ -25,17 +25,21 @@ def surface_density(segmentation, voxelsize_mm, sond_raster_mm=None, aoi=None):
     axis = 0
     if sond_raster_mm is None:
         sond_raster_mm = voxelsize_mm
+    if aoi is None:
+        aoi = np.ones(segmentation.shape)
+
     im_edg = find_edge(segmentation, axis=axis)
-    im_sond = bufford_needle_sond(
-        im_edg, voxelsize_mm, sond_raster_mm, axis=axis)
+    im_edg = im_edg * aoi
+    im_sond, aoi_sond = bufford_needle_sond(
+        im_edg, voxelsize_mm, sond_raster_mm, axis=axis, aoi=aoi)
 
     # est S = 2 \frac{1}{n} \sum_{i=1}^{n} \frac{v}{l_i} \cdot l_i
 
 # celkova delka sond
-    n_needle = (im_sond.shape[1] * im_sond.shape[2])
-    one_needle_l = im_sond.shape[0] * voxelsize_mm[0]
-    length = n_needle * one_needle_l
-
+    # n_needle = (im_sond.shape[1] * im_sond.shape[2])
+    # one_needle_l = im_sond.shape[0] * voxelsize_mm[0]
+    # length = n_needle * one_needle_l
+    length = np.sum(aoi_sond > 0) * voxelsize_mm[0]
 
 # inverse of the probe per unit volume v/l_i
     # ippuv = (
@@ -44,12 +48,12 @@ def surface_density(segmentation, voxelsize_mm, sond_raster_mm=None, aoi=None):
     #     (sond_raster_mm[axis] * im_sond.shape[axis])
     # )
 # Pocet pruseciku
+    # Ii = np.sum(np.abs(im_sond))
     Ii = np.sum(np.abs(im_sond))
 
     # import sed3
     # ed = sed3.sed3(im_sond)
     # ed.show()
-
 
     # Kubinova2001
     # print "Ii = ", Ii
@@ -73,12 +77,12 @@ def find_edge(segmentation, axis):
     return retval
 
 
-def bufford_needle_sond(data3d, voxelsize_mm, raster_mm, axis):
+def bufford_needle_sond(data3d, voxelsize_mm, raster_mm, axis, aoi):
     # TODO implement needle
     # retval = data3d[::2, ::2, ::2]
     retval = data3d
 
-    return retval
+    return retval, aoi
 
 
 def main():
