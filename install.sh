7d04c09a01a9f2244eb05cac37598c21dfa36a1d
M. Jirik <mjirik@kky.zcu.cz>
2015-03-16 16:24:33 +0100

    install update

---
+++ install.sh
@@ -0,0 +1,33 @@
+conda install --yes --file requirements_conda.txt
+
+# 2. easy_install requirements simpleITK  
+easy_install -U --user SimpleITK mahotas
+
+# 3. pip install our packages pyseg_base and dicom2fem
+pip install -r requirements_pip.txt --user
+
+mkdir projects
+
+# 4. install gco_python
+
+cd projects
+# mkdir gco_python
+# cd gco_python
+git clone https://github.com/mjirik/gco_python.git 
+cd gco_python
+# echo `pwd`
+make
+python setup.py install --user
+cd ..
+cd ..
+
+
+# 5. skelet3d - optional for Histology Analyser
+# sudo -u $USER cd ~/projects
+# mkdir ~/projects/skelet3d
+# mkdir /projects/skelet3d
+git clone https://github.com/mjirik/skelet3d.git 
+cd skelet3d
+mkdir build
+cmake ..
+make
