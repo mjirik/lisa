1866b3a015015495a279f202da6d29b661fa852b
Miroslav Jiřík <miroslav.jirik@gmail.com>
2015-04-06 19:38:28 +0200

    osx support

--- ubuntu_installer.sh
+++ ubuntu_installer.sh
@@ -6,7 +6,32 @@
 #   any other argument: devel version with https is used
 NARGS=$#
 ARG1=$1
+cd ~
+HOMEDIR="`pwd`"
+USER="$(echo `pwd` | sed 's|.*home/\([^/]*\).*|\1|')"
 
+echo "installing for user:"
+echo "$USER"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # ...
+    wget https://raw.githubusercontent.com/mjirik/lisa/master/requirements_apt.txt -O requirements_apt.txt
+
+    sudo apt-get install -y -qq $(grep -vE "^\s*#" requirements_apt.txt | tr "\n" " ")
+    
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
 # echo "$ARG1"
 # if [ "$ARG1" = "" ] ; then
 #     echo "asdfa"
@@ -19,12 +44,7 @@ ARG1=$1
 #     echo "Cloning unstable branch using http"
 # fi
 # exit
-cd ~
-HOMEDIR="`pwd`"
-USER="$(echo `pwd` | sed 's|.*home/\([^/]*\).*|\1|')"
 
-echo "installing for user:"
-echo "$USER"
 
 # apt-get update
 # apt-get upgrade -y
@@ -32,9 +52,7 @@ echo "$USER"
 # 0. deb package requirements
 # sudo -u $USER pip install wget --user
 # sudo -u $USER python -m wget https://raw.githubusercontent.com/mjirik/lisa/master/requirements_apt.txt
-wget https://raw.githubusercontent.com/mjirik/lisa/master/requirements_apt.txt -O requirements_apt.txt
 
-sudo apt-get install -y -qq $(grep -vE "^\s*#" requirements_apt.txt | tr "\n" " ")
 # apt-get install -y python git python-dev g++ python-numpy python-scipy python-matplotlib python-sklearn python-skimage python-dicom cython python-yaml sox make python-qt4 python-vtk python-setuptools curl python-pip cmake
 
 # 1. conda python packages
@@ -43,25 +61,37 @@ if hash conda 2>/dec/null; then
 else
     touch ~/.bashrc
     MACHINE_TYPE=`uname -m`
-    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-        echo "Installing 64-bit conda"
-    # 64-bit stuff here
-        wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O Miniconda-latest-Linux-x86_64.sh
-        bash Miniconda-latest-Linux-x86_64.sh -b
-    else
-    # 32-bit stuff here
-        echo "Installing 32-bit conda"
-        wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86.sh -O Miniconda-latest-Linux-x86.sh
-        bash Miniconda-latest-Linux-x86.sh -b
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+            echo "Installing 64-bit conda"
+        # 64-bit stuff here
+            wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O Miniconda-latest-Linux-x86_64.sh
+            bash Miniconda-latest-Linux-x86_64.sh -b
+        else
+        # 32-bit stuff here
+            echo "Installing 32-bit conda"
+            wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86.sh -O Miniconda-latest-Linux-x86.sh
+            bash Miniconda-latest-Linux-x86.sh -b
+        fi
+        wget https://raw.githubusercontent.com/mjirik/lisa/master/install.sh -O install.sh
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        curl "http://repo.continuum.io/miniconda/Miniconda-latest-MacOSX-x86_64.sh" -o "Miniconda-latest.sh"
+        bash Miniconda-latest.sh -b
+        echo "export PATH=$HOMEDIR/miniconda/bin:\$PATH" >> ~/.profile
+        
+        curl https://raw.githubusercontent.com/mjirik/lisa/master/install.sh -o install.sh
     fi
     export PATH=$HOMEDIR/miniconda/bin:$PATH
     conda
 fi
 
-wget https://raw.githubusercontent.com/mjirik/lisa/master/install.sh -O install.sh
+
 bash install.sh
 
-
+cd 
+cd projects
+cd skelet3d
+cd build 
 sudo make install
 # sudo -u $USER sh -c "cd ~/projects/skelet3d/build && cmake .. && make"
 # sudo -u $USER mkdir build
