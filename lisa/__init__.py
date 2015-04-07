5f1be3b65c712789871cac850cba21b2cf78256b
M. Jirik <mjirik@kky.zcu.cz>
2015-04-07 10:33:54 +0200

    import sss in init

--- lisa/__init__.py
+++ lisa/__init__.py
@@ -1,8 +1,10 @@
-__all__ = ['organ_segmentation', 'qmisc', 'misc', 'experiments']
+__all__ = ['organ_segmentation', 'qmisc', 'misc', 'experiments',
+           'support_structure_segmentation']
 import organ_segmentation
 import qmisc
 import misc
 import experiments
+import support_structure_segmentation
 # from import Model, ImageGraphCut
 # from seed_editor_qt import QTSeedEditor
 # from dcmreaddata import DicomReader
