a8066a2545788484a74be696ad1152b0d9a4944f
M. Jirik <mjirik@kky.zcu.cz>
2015-04-15 12:32:24 +0200

    spline length fixed, pep8

--- tests/skeleton_test.py
+++ tests/skeleton_test.py
@@ -19,6 +19,34 @@ import copy
 class TemplateTest(unittest.TestCase):
 
     @attr('actual')
+    def test_length_types(self):
+        data = np.zeros([20, 20, 20], dtype=np.int8)
+        # snake
+        data[18, 8:10, 13] = 1
+        data[18, 10:12, 13] = 1
+        data[18, 12:14, 13] = 1
+        data[18, 14:17, 13] = 1
+        data[18, 17, 14:17] = 1
+        data[14:18, 17, 17] = 1
+
+        skel = data
+
+        skan = sk.SkeletonAnalyser(copy.copy(skel), volume_data=data,
+                                   voxelsize_mm=[1, 20, 300])
+        skan.spline_smoothing = 5
+        vessel_tree = skan.skeleton_analysis()
+        pixel = vessel_tree[1]['lengthEstimationPixel']
+        poly = vessel_tree[1]['lengthEstimationPoly']
+        spline = vessel_tree[1]['lengthEstimationSpline']
+
+        # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
+        # self.assertAlmostEqual
+        # self.assertAlmostEqual(vessel_tree[1]['lengthEstimationPixel'], 10)
+        # self.assertAlmostEqual(vessel_tree[2]['lengthEstimationPixel'], 200)
+        # self.assertAlmostEqual(vessel_tree[3]['lengthEstimationPixel'], 3000)
+        # self.assertAlmostEqual(vessel_tree[1]['lengthEstimationPixel'],
+        #                        diag_length)
+
     def test_length(self):
 
         data = np.zeros([20, 20, 20], dtype=np.int8)
@@ -51,7 +79,6 @@ class TemplateTest(unittest.TestCase):
         diag_length = 2 * ((1**2 + 20**2 + 300**2)**0.5)
         self.assertAlmostEqual(vessel_tree[4]['lengthEstimationPixel'],
                                diag_length)
-        # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
 
     def test_tortuosity(self):
         import skelet3d
