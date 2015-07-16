d002867a69196bda6595e7393a3d1d84f532b199
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-07-16 14:17:28 +0200

    unbiased brick in progress

--- lisa/data_manipulation.py
+++ lisa/data_manipulation.py
@@ -21,28 +21,37 @@ import qmisc
 
 def unbiased_brick_filter(binary_data, crinfo):
     """
+    return only binary object which suits with unbiased brick 
+
     :param data: 3D ndimage data
     :param crinfo: crinfo 
     http://www.stereology.info/the-optical-disector-and-the-unbiased-brick/
     """
+    binary_data = binary_data.copy()
     crinfo = qmisc.fix_crinfo(crinfo)
+
+    imlab, num_features = scipy.ndimage.measurements.label(binary_data)
+
+    brick_neg_mask = np.zeros(binary_data.shape, dtype=np.uint8)
+    brick_neg_mask[
+            crinfo[0][0]:crinfo[0][1],
+            crinfo[1][0]:crinfo[1][1],
+            crinfo[2][0]:crinfo[2][1]
+        ] = 1
 
     exclude_mask = np.zeros(binary_data.shape, dtype=np.uint8)
     exclude_mask[:,:, crinfo[2][1]:] = 1
     exclude_mask[:,crinfo[1][1]:, crinfo[2][0]:] = 1
     exclude_mask[:crinfo[0][0],crinfo[1][0]:, crinfo[2][0]:] = 1
 
-    imlab, num_features = scipy.ndimage.measurements.label(binary_data)
+    # remove what is not in touch with brick
+    imlab = keep_what_is_in_touch_with_mask(
+            imlab, brick_neg_mask, max_label=num_features)
+    # remove what is in touch with exclude
+    imlab = remove_what_is_in_touch_with_mask(imlab, exclude_mask)
 
 
-    datatmp = imlab * exclude_mask
-    nz = np.nonzero(datatmp)
-    labels_to_exclude = imlab[nz[0], nz[1], nz[2]]
-    labels_to_exclude = np.unique(labels_to_exclude)
 
-
-    for lab in labels_to_exclude:
-        imlab[imlab == lab] = 0
 
     return (imlab > 0).astype(binary_data.dtype)
     
@@ -53,6 +62,33 @@ def unbiased_brick_filter(binary_data, crinfo):
     #     __int_or_none(crinfo[1][0]):__int_or_none(crinfo[1][1]),
     #     __int_or_none(crinfo[2][0]):__int_or_none(crinfo[2][1])
     #     ]
+
+def keep_what_is_in_touch_with_mask(imlab, keep_mask, max_label):
+    datatmp = imlab * keep_mask
+    nz = np.nonzero(datatmp)
+    labels_to_keep = imlab[nz[0], nz[1], nz[2]]
+    labels_to_keep = np.unique(labels_to_keep)
+
+    for lab in range(0, max_label):
+        if lab in labels_to_keep:
+            pass
+        else:
+            imlab[imlab == lab] = 0
+
+    return imlab
+
+def remove_what_is_in_touch_with_mask(imlab, exclude_mask):
+    datatmp = imlab * exclude_mask
+    nz = np.nonzero(datatmp)
+    labels_to_exclude = imlab[nz[0], nz[1], nz[2]]
+    labels_to_exclude = np.unique(labels_to_exclude)
+
+
+    for lab in labels_to_exclude:
+        imlab[imlab == lab] = 0
+
+    return imlab
+
 
 
 def main():
--- tests/data_manipulation_test.py
+++ tests/data_manipulation_test.py
@@ -12,29 +12,62 @@
 import unittest
 from nose.plugins.attrib import attr
 import numpy as np
+import scipy
 import lisa.data_manipulation as dama
 
-class TemplateTest(unittest.TestCase):
+class DataManipulationTest(unittest.TestCase):
 
     @attr('actual')
     def test_unbiased_brick(self):
+        """
+        Test unbiased brick. There sould be correct number of object in
+        sum of subbrick.
+        """
 # import np.random
-        shp = [30,32,34]
+        shp = [40, 42, 44]
         data = np.zeros(shp, dtype=np.uint8)
-        data[11:17, 11:13, 14:15] = 1
-        data[6:14, 14:16, 14:15] = 1
-        data[14:16, 11:18, 16:18] = 1
-        data[16:18, 13:16, 18:23] = 1
-        data[13:15, 11:16, 7:12] = 1
-        data[16:18, 7:12, 16:18] = 1
+        data[14:18, 22, 17] = 1
+        data[12:16, 15, 15] = 1
+        data[13:18, 18, 13] = 1
+        # data[26:29, 21, 18] = 1
+        # data[21, 23:26, 25] = 1
+        # data[21, 17:21, 24] = 1
+        # data[16, 26:29, 21] = 1
+        # data[24, 28, 13:23] = 1
+        # data[19, 28, 12:16] = 1
+        # data[19, 15, 22:26] = 1
+
+
         # import sed3
         # se = sed3.sed3(data)
         # se.show()
-        # data += (np.random.random(shp) * 50).astype(np.int16)
-        outputdata = dama.unbiased_brick_filter(data, [[10,20],[10,20],[10,20]])
-        import sed3
-        se = sed3.sed3(outputdata)
-        se.show()
+
+# pokud to pocita dobr
+        sh = [None] * 8
+        sh[0] = [[10,20],[10,20],[10,20]]
+        sh[1] = [[10,20],[10,20],[20,30]]
+        sh[2] = [[10,20],[20,30],[10,20]]
+        sh[3] = [[10,20],[20,30],[20,30]]
+        sh[4] = [[20,30],[10,20],[10,20]]
+        sh[5] = [[20,30],[10,20],[20,30]]
+        sh[6] = [[20,30],[20,30],[10,20]]
+        sh[7] = [[20,30],[20,30],[20,30]]
+
+        suma = 0
+
+        for shi in sh:
+
+            outputdata = dama.unbiased_brick_filter(data, shi)
+            imlab, num_features = scipy.ndimage.measurements.label(outputdata)
+            print num_features
+            suma += num_features
+
+        print "for is over"
+
+        print suma
+        # import ipdb; ipdb.set_trace() #  noqa BREAKPOINT
+
+
         
 
 if __name__ == "__main__":
