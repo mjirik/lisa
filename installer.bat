8e6a62eb93cc9fda4fdb58f7ce10458499ed4b4a
Miroslav Jiřík <miroslav.jirik@gmail.com>
2015-11-22 19:22:36 +0100

    call activate in installer sript

--- installer.bat
+++ installer.bat
@@ -1,6 +1,6 @@
-conda create --no-default-packages -n lisa pip pywget numpy
+conda create -y --no-default-packages -n lisa pip pywget numpy
 
-activate lisa
+call activate lisa
 python -m wget https://raw.githubusercontent.com/mjirik/lisa/master/requirements_pip.txt
 pip install -r requirements_pip.txt
 conda install -y -c SimpleITK -c mjirik lisa
