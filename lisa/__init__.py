bbd9074064187581935e69aa1d296875a4fa2f20
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-11-22 10:42:11 +0100

    smaller init

--- lisa/__init__.py
+++ lisa/__init__.py
@@ -1,10 +1,11 @@
-__all__ = ['organ_segmentation', 'qmisc', 'misc', 'experiments',
-           'support_structure_segmentation']
+__all__ = ['organ_segmentation']
+#, 'qmisc', 'misc', 'experiments',
+           #'support_structure_segmentation']
 import organ_segmentation
 # import qmisc
 # import misc
-import experiments
-import support_structure_segmentation
+# import experiments
+# import support_structure_segmentation
 # from import Model, ImageGraphCut
 # from seed_editor_qt import QTSeedEditor
 # from dcmreaddata import DicomReader
