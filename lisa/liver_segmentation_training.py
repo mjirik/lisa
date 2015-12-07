e9db1511c99bab851a4fda9d7ddb5e392d59d8be
Miroslav Jiřík <mjirik@kky.zcu.cz>
2015-12-07 12:27:55 +0100

    new training

---
+++ lisa/liver_segmentation_training.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR%  <>
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
+import argparse
+
+
+
+
+def main():
+    logger = logging.getLogger()
+
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    logger.addHandler(ch)
+
+    # create file handler which logs even debug messages
+    # fh = logging.FileHandler('log.txt')
+    # fh.setLevel(logging.DEBUG)
+    # formatter = logging.Formatter(
+    #     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # fh.setFormatter(formatter)
+    # logger.addHandler(fh)
+    # logger.debug('start')
+
+    # input parser
+    parser = argparse.ArgumentParser(
+        description=__doc__
+    )
+    # parser.add_argument(
+    #     '-i', '--inputfile',
+    #     default=None,
+    #     required=True,
+    #     help='input file'
+    # )
+    parser.add_argument(
+        '-o', '--outputfile',
+        default="liver.ol.p",
+        help='output file'
+    )
+    parser.add_argument(
+        '-d', '--debug', action='store_true',
+        help='Debug mode')
+    args = parser.parse_args()
+
+    if args.debug:
+        ch.setLevel(logging.DEBUG)
+    train_liver_localizator_from_sliver_data(args.outputfile)
+
+
+if __name__ == "__main__":
+    main()
