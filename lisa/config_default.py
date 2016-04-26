8029ee1191858298062035ba7a08057c01749d35
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-04-26 15:20:52 +0200

    3mm default option added

--- lisa/config_default.py
+++ lisa/config_default.py
@@ -44,6 +44,19 @@ CONFIG_DEFAULT={
             },
             'working_voxelsize_mm': 2
         },
+        "simple 3 mm":{
+            "segmodelparams": {
+                'mdl_stored_file': None,
+                'fv_type': 'intensity',
+                'fv_extern': None,
+                # 'mdl_stored_file': '~/lisa_data/liver_intensity.Model.p',
+                # "fv_type": 'fv_extern',
+                # 'fv_extern':'intensity_localization_fv',
+                # 'method': 'multiscale_graphcut',
+                "params": {"covariance_type": "full", "n_components": 3}
+            },
+            'working_voxelsize_mm': 3
+        },
         "label kidney L":{
             "output_label": 15,
         },
