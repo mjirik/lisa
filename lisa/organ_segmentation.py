b2df65342c6305e46222badb20d3d37ba7c76cb1
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-05-26 02:12:36 +0200

    Bump version: 1.8.17 → 1.8.18

--- lisa/organ_segmentation.py
+++ lisa/organ_segmentation.py
@@ -229,7 +229,7 @@ class OrganSegmentation():
         self.lisa_operator_identifier = lisa_operator_identifier
         # self.version = qmisc.getVersionString()
         # if self.version is None:
-        self.version = "1.8.17"
+        self.version = "1.8.18"
         self.viewermax = viewermax
         self.viewermin = viewermin
         self.volume_unit = volume_unit
