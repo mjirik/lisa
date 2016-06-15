d20dab043399dc01dbe52ea44afacf9d49e4c05a
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-06-15 09:56:41 +0200

    method name fixed to PV

--- tests/vessels_segmentation_test.py
+++ tests/vessels_segmentation_test.py
@@ -120,7 +120,7 @@ class VesselsSegmentationTest(unittest.TestCase):
                  'voxelsize_mm': voxelsize_mm, 'slab': slab}
 
         datap = vr.resection(
-            datap, method=True,
+            datap, method='PV',
             interactivity=False, seeds=seeds
         )
 
