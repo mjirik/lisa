ca33739b111027d245115989b020c2bdc80cd0a5
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-11-21 21:00:54 +0100

    Bump version: 1.9.18 → 1.9.19

--- lisa/__init__.py
+++ lisa/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['organ_segmentation']
 #, 'qmisc', 'misc', 'experiments',
            #'support_structure_segmentation']
-__version__ = "1.9.18"
+__version__ = "1.9.19"
 
 import organ_segmentation
 from main import lisa_main
