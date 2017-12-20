afd922d644a2ac0c27ee6c88598614aae6365f9a
Miroslav Jiřík <miroslav.jirik@gmail.com>
2017-12-20 18:47:18 +0100

    add forge channel

--- installer.bat
+++ installer.bat
@@ -1,6 +1,6 @@
 set PATH=%PATH%;%HOMEPATH%\Miniconda2;%HOMEPATH%\Miniconda2\Scripts;C:\Miniconda2\Scripts;C:\Miniconda2
 rem :pygco is not compiled for osx now this is why it is not in meta.yaml
-conda create -y -c mjirik -c SimpleITK -c menpo --no-default-packages -n lisa pywget wget pip lisa pygco
+conda create -y -c mjirik -c SimpleITK -c menpo -c conda-forge --no-default-packages -n lisa pywget wget pip lisa pygco
 
 call activate lisa
 rem :windows specific
