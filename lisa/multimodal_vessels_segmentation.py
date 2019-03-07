23395de469a8517bd06416b1e58dfc95e2b5cf3d
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-03-07 18:59:15 +0100

    multimodal vessel segmentation added

---
+++ lisa/multimodal_vessels_segmentation.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+# import funkcí z jiného adresáře
+import sys
+import os.path
+import io3d
+path_to_script = os.path.dirname(os.path.abspath(__file__))
+#import featurevector
+import unittest
+
+import logging
+logger = logging.getLogger(__name__)
+
+# import apdb
+# apdb.set_trace();
+# import scipy.io
+import numpy as np
+
+# ----------------- my scripts --------
+import argparse
+from . import segmentation_general
+
+class MultimodalVesselSegmentation:
+    def __init__(self):
+        pass
+
+    def set_params(self):
+        pass
+
+    def run(self):
+        pass
+
+    def get_output(self):
+        pass
+
+
+
